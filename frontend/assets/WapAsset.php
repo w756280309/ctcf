9511e7859987e28064ac7be43861e1a4aa64545c
zuoyuwei <zuoyuwei@wangcaigu.com>
2016-01-11 10:43:24 +0800

    短信测试；定时任务测试

---
+++ frontend/assets/WapAsset.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xhy
+ * Date: 15-3-11
+ * Time: 上午10:53
+ */
+
+namespace frontend\assets;
+
+use yii\web\AssetBundle;
+
+
+class WapAsset extends AssetBundle{
+
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        'css/bootstrap.min.css',
+        'css/base.css',
+    ];
+    public $js = [
+        //'js/jquery.js'
+    ];
+    public $depends = [
+       // 'backend\assets\AppAsset.php',
+    ];
+
+}
