4f95d499734a0cdb92e3d10844661a9bbb9677ed
zhy9001 <zhanghongyu@wangcaigu.com>
2016-01-12 09:12:02 +0800

    短信白名单

--- lib/SmsGate/SmsRequest.php
+++ lib/SmsGate/SmsRequest.php
@@ -12,11 +12,26 @@ use common\models\sms\SmsMessage;
  */
 class SmsRequest {
 
+    private $isTest = true;//true开启,false关闭
+    private $testMobile = ['15810036547','18518154492'];//白名单数组
+    
+    public function getIsTest() {
+        return $this->isTest;
+    }
+
+    public function getTestMobile() {
+        return $this->testMobile;
+    }
+
     /**
      * 发送短信方法
      * @param SmsMessage 对象
      */
     public function send(SmsMessage $message) {
+        $mobile = $message->mobile;
+        if ($this->isTest && !empty($this->testMobile)) {
+            $mobile = current($this->testMobile);
+        }
         $sms = new Sms();
         $msg_arr = json_decode($message->message, false);
         $data = $sms->sendTemplateSMS($message->mobile, $msg_arr, $message->template_id);
