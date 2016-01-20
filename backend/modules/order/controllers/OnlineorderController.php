b5f2899b27c447d7617914d3c854535156267208
zuoyuwei <zuoyuwei@wangcaigu.com>
2016-01-20 10:59:39 +0800

    修改格式

--- backend/modules/order/controllers/OnlineorderController.php
+++ backend/modules/order/controllers/OnlineorderController.php
@@ -14,15 +14,17 @@ use backend\modules\user\core\v1_0\UserAccountBackendCore;
 /**
  * OrderController implements the CRUD actions for OfflineOrder model.
  */
-class OnlineorderController extends BaseController {
-
-    public function actionList($id=null) {//取出金额总计
+class OnlineorderController extends BaseController
+{
+    public function actionList($id = null)
+    {
+        //取出金额总计
         $username = Yii::$app->request->get('username');
         $mobile = Yii::$app->request->get('mobile');
-        
+
         $query = OnlineOrder::find()->where(['online_pid' => $id]);
         $data = clone $query;
-        
+
         //已筹集金额
         $moneyTotal = $query->sum('order_money');
         //剩余可投金额
@@ -31,18 +33,18 @@ class OnlineorderController extends BaseController {
         //已投资人数
         $count = $query->groupBy('uid')->count();
         //募捐时间
-        $time = ( time() - ($biao->start_date)); //秒数
+        $time = (time() - ($biao->start_date)); //秒数
         $day = floor($time / (24 * 3600)); //天数
         $hour = floor(($time - $day * 24 * 3600) / 3600); //小时
         $mintus = floor(($time - $day * 24 * 3600 - $hour * 60 * 60) / 60); //分钟 都是余数！！！！
-        $mujuanTime = "    " . $day . "   天   " . $hour . "   小时   " . $mintus . "   分";
-        
-        if(!empty($username)) {
-            $data->andFilterWhere(['like','username', $username]);
+        $mujuanTime = '    '.$day.'   天   '.$hour.'   小时   '.$mintus.'   分';
+
+        if (!empty($username)) {
+            $data->andFilterWhere(['like', 'username', $username]);
         }
-        
-        if(!empty($mobile)) {
-            $data->andFilterWhere(['like','mobile', $mobile]);
+
+        if (!empty($mobile)) {
+            $data->andFilterWhere(['like', 'mobile', $mobile]);
         }
 
         //正常显示详情页
@@ -55,21 +57,22 @@ class OnlineorderController extends BaseController {
                     'moneyTotal' => $moneyTotal,
                     'shengyuKetou' => $shengyuKetou,
                     'renshu' => $count,
-                    'mujuanTime' => $mujuanTime
+                    'mujuanTime' => $mujuanTime,
         ]);
     }
 
-    public function actionDetailr($id=null,$type=null) {
+    public function actionDetailr($id = null, $type = null)
+    {
         $status = Yii::$app->request->get('status');
         $time = Yii::$app->request->get('time');
         $title = Yii::$app->request->get('title');
-        
+
         $query = OnlineProduct::find()->where(['del_status' => OnlineProduct::STATUS_USE, 'borrow_uid' => $id]);
-        if(!empty($status)) {
+        if (!empty($status)) {
             $query->andWhere(['status' => $status]);
         }
-        if(!empty($title)) {
-            $query->andFilterWhere(['like','title',$title]);
+        if (!empty($title)) {
+            $query->andFilterWhere(['like', 'title', $title]);
         }
 //        if (isset($status) && $status !== '' && !empty($title)) {
 //            $query = "del_status=0 and status='$status' and borrow_uid=$id and title like '%$title%'";
@@ -80,8 +83,8 @@ class OnlineorderController extends BaseController {
 //        }
 //        $query = OnlineProduct::find()->where($query);
         if (!empty($time)) {
-            $query->andFilterWhere(['<', 'created_at', strtotime($time . " 23:59:59")]);
-            $query->andFilterWhere(['>=', 'created_at', strtotime($time . " 0:00:00")]);
+            $query->andFilterWhere(['<', 'created_at', strtotime($time.' 23:59:59')]);
+            $query->andFilterWhere(['>=', 'created_at', strtotime($time.' 0:00:00')]);
         }
         //正常显示详情页
         $pages = new Pagination(['totalCount' => $query->count(), 'pageSize' => '10']);
@@ -108,12 +111,13 @@ class OnlineorderController extends BaseController {
         ]);
     }
 
-    public function actionDetailt($id = null, $type = null) {
-        $status = Yii::$app->request->get('status'); 
+    public function actionDetailt($id = null, $type = null)
+    {
+        $status = Yii::$app->request->get('status');
         $time = Yii::$app->request->get('time');
-        
+
         $query = OnlineOrder::find()->where(['uid' => $id]);
-        if($status != null) {
+        if ($status != null) {
             $query->andWhere(['status' => $status]);
         }
 //        if (isset($status) && $status !== '') {
@@ -123,8 +127,8 @@ class OnlineorderController extends BaseController {
 //        }
        // $query = OnlineOrder::find()->where($query);
         if (!empty($time)) {
-            $query->andFilterWhere(['<', 'created_at', strtotime($time . " 23:59:59")]);
-            $query->andFilterWhere(['>=', 'created_at', strtotime($time . " 0:00:00")]);
+            $query->andFilterWhere(['<', 'created_at', strtotime($time.' 23:59:59')]);
+            $query->andFilterWhere(['>=', 'created_at', strtotime($time.' 0:00:00')]);
         }
         //正常显示详情页
         $pages = new Pagination(['totalCount' => $query->count(), 'pageSize' => '10']);
@@ -140,23 +144,23 @@ class OnlineorderController extends BaseController {
 //        $failureNum = OnlineOrder::find()->where(['uid' => $id, 'status' => 2])->count();
 //        //项目名称
 //        $online_ids = OnlineOrder::find()->where(['uid' => $id])->select("id,online_pid")->asArray()->all();
-        
+
         $moneyTotal = 0;  //提现总额
         $successNum = 0;  //成功笔数
         $failureNum = 0;  //失败笔数
         $numdata = OnlineOrder::find()->where(['uid' => $id])->select('id,order_money,online_pid,status')->asArray()->all();
         $bc = new BcRound();
         bcscale(14);
-        foreach ($numdata as $data){
-            $moneyTotal = bcadd($moneyTotal,$data['order_money']);
-            if($data['status']==OnlineOrder::STATUS_SUCCESS){
-                $successNum++;
-            }else if($data['status']==OnlineOrder::STATUS_CANCEL){
-                $failureNum++;
+        foreach ($numdata as $data) {
+            $moneyTotal = bcadd($moneyTotal, $data['order_money']);
+            if ($data['status'] == OnlineOrder::STATUS_SUCCESS) {
+                ++$successNum;
+            } elseif ($data['status'] == OnlineOrder::STATUS_CANCEL) {
+                ++$failureNum;
             }
         }
-        $moneyTotal = $bc->bcround($moneyTotal,2);
-        
+        $moneyTotal = $bc->bcround($moneyTotal, 2);
+
         //以online_order表中的id为下标online_id为单元值，组成一维数组        
         $result = OnlineProduct::findBySql("select oo.id,title from online_product op  left join online_order oo  on op.id=oo.online_pid where  oo.uid=$id")->asArray()->all();
         $res = [];
@@ -176,5 +180,4 @@ class OnlineorderController extends BaseController {
                     'failureNum' => $failureNum,
         ]);
     }
-
 }
