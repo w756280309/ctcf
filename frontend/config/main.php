767747c755cec7870a86eb0fe7c93e1b383b2dc8
Mo Di <modi@wangcaigu.com>
2016-01-22 10:16:53 +0800

    改用1.11.x版本的jquery

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -1,16 +1,17 @@
 <?php
+
 $params = array_merge(
-    require(__DIR__ . '/../../common/config/params.php'),
-    require(__DIR__ . '/../../common/config/params-local.php'),
-    require(__DIR__ . '/params.php'),
-    require(__DIR__ . '/params-local.php')
+    require(__DIR__.'/../../common/config/params.php'),
+    require(__DIR__.'/../../common/config/params-local.php'),
+    require(__DIR__.'/params.php'),
+    require(__DIR__.'/params-local.php')
 );
 
 return [
     'id' => 'app-frontend',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
-    'language'=>'zh-CN',
+    'language' => 'zh-CN',
     'controllerNamespace' => 'frontend\controllers',
     'components' => [
         'user' => [
@@ -36,11 +37,14 @@ return [
                     'css' => [],
                 ],
                 'yii\bootstrap\BootstrapPluginAsset' => [
-                    'js'=>[]
+                    'js' => [],
                 ],
-//                'yii\web\JqueryAsset' => [
-//                    'js'=>[]
-//                ],
+                'yii\web\JqueryAsset' => [
+                    'sourcePath' => '@app/assets',
+                    'js' => [
+                        'vendor/jquery/dist/jquery.min.js',
+                    ],
+                ],
             ],
 
         ],
