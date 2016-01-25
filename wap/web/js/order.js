13de0d4cdc4894a0327c9e7ca50475995e718928
Mo Di <modi@wangcaigu.com>
2016-01-25 18:13:46 +0800

    修正：投标购买按钮重复点击，ajax请求失败的情况

--- wap/web/js/order.js
+++ wap/web/js/order.js
@@ -1,36 +1,39 @@
-﻿/**
+/**
  * Created by lcl on 2015/11/16.
  */
 var csrf;
 $(function(){
-   csrf = $("meta[name=csrf-token]").attr('content');
-   $('#buybtn').bind('click',function(){
-       if($('#money').val()==''){
-           toast(this,'投资金额不能为空');
-                    return false;
-       }
-       if($('#trade_pwd').val()==''){
-           toast(this,'交易密码不能为空');
-                    return false;
-       }
-       vals = $("#orderform").serialize();
-       $(this).attr('disabled', true);
-       $.post($("#orderform").attr("action"), vals, function (data) {
-           $(this).attr('disabled', false);
-           if(data.code==0){
+    csrf = $("meta[name=csrf-token]").attr('content');
+    var $buy = $('#buybtn');
+    $buy.on('click', function() {
+        if ($('#money').val() == '') {
+            toast(this, '投资金额不能为空');
+            return false;
+        }
+        if($('#trade_pwd').val() == '') {
+            toast(this, '交易密码不能为空');
+            return false;
+        }
+        $buy.attr('disabled', true);
+        var vals = $("#orderform").serialize();
+        var xhr = $.post($("#orderform").attr("action"), vals, function (data) {
+            if (data.code == 0) {
                toastWithMastUrl('/user/user/myorder','购买成功')
-           }else{
+            } else {
                toast(this,data.message);
-           }
+            }
 
-           if(data.tourl!=undefined){
-               location.href=data.tourl;
+            if (data.tourl != undefined) {
+                location.href=data.tourl;
             }
         });
-   });
-   $('#money').blur(function(){
-       money = $(this).val();
-       if (isNaN(money)) {
+        xhr.always(function() {
+            $buy.attr('disabled', false);
+        })
+    });
+    $('#money').on('blur', function() {
+        money = $(this).val();
+        if (isNaN(money)) {
             toast(this,'交易密码不能为空');
             money = 0;
             $(this).focus();
