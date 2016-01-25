5bd3d9251c30394f486d2245ce69b71c490c37d1
zuoyuwei <zuoyuwei@wangcaigu.com>
2016-01-25 15:08:56 +0800

    修改数据库jiaxi字段

--- db/20160125-jiaxi.sql
+++ db/20160125-jiaxi.sql
@@ -1,2 +1,5 @@
 alter table `online_product` 
    add column `jiaxi` decimal(6,4) NULL COMMENT '加息利率（%）' after `yield_rate`;
+
+alter table `online_product` 
+   change `jiaxi` `jiaxi` decimal(3,1) NULL  comment '加息利率（%）';
