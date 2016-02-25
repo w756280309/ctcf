55d5cc3b51d9642f1ed46374d8f0a247fc1b7c71
zuoyuwei <zuoyuwei@wangcaigu.com>
2016-02-25 19:34:22 +0800

    修改股权投资页面

--- wap/modules/order/views/booking/detail.php
+++ wap/modules/order/views/booking/detail.php
@@ -7,7 +7,7 @@ $this->title = '股权投资';
 <link href="/css/first.css" rel="stylesheet"/>
 
 <div class="container" style="background: #fff;">
-        <div class="title-div color1">同信贝湾股权投资资金三期</div>
+        <div class="title-div color1"><?= $model->name ?></div>
 
         <div class="row" style="margin:5px auto;">
             <div class="col-xs-2 col-sm-2" ></div>
@@ -45,20 +45,20 @@ $this->title = '股权投资';
                 <li >
                     <div class="color2 p1">
                         <a href="javascript:;" class="a1 bg1" ></a>
-                        <p><span class="color1">基金管理人:</span>本基⾦管理⼈人为深圳贝湾⾦金融服务有限公司，本基⾦为母基⾦，全额投资于同信投资有限责任公司(西藏同信证券股份有限公司全资券商直投⼦子公司)管理的基⾦</p>
+                        <p><span class="color1">基金管理人:</span>本基⾦管理人为深圳贝湾⾦金融服务有限公司，本基⾦为母基⾦，全额投资于同信投资有限责任公司(西藏同信证券股份有限公司全资券商直投子公司)管理的基⾦</p>
                     </div>
                 </li>
                 <li >
                     <div class="color2 p1">
                         <a href="javascript:;" class="a1 bg2" ></a>
-                        <p><span class="color1">投资方向:</span>本基⾦全额投资于“同信居莫愁股权投资基”该基⾦投资于公寓⾏行业中具有高成长性心价值的拟挂牌新三板的企业</p>
+                        <p><span class="color1">投资方向:</span>本基⾦全额投资于“同信居莫愁股权投资基金”,该基⾦投资于公寓⾏行业中具有高成长性、核心价值的拟挂牌新三板的企业</p>
                     </div>
                 </li>
 
                 <li >
                     <div class="color2 p1">
                         <a href="javascript:;" class="a1 bg3" ></a>
-                        <p><span class="color1">退出方式:</span>同信居莫愁股权投资基金”退出而出，退出方式为挂板新三板退出、IPO 退出、并购退出、原股东回购</p>
+                        <p><span class="color1">退出方式:</span>随"同信居莫愁股权投资基金”退出而出，退出方式为挂板新三板退出、IPO 退出、并购退出、原股东回购</p>
                     </div>
                 </li>
 
@@ -71,7 +71,7 @@ $this->title = '股权投资';
                 <li >
                     <div class="color2 p1">
                         <a href="javascript:;" class="a1 bg5" ></a>
-                        <p><span class="color1">业绩提成:</span>“同信居莫愁股权投资基金二期”年化收益不低于20%的情形下，同信投资有限责任公司按照基金的 10%收取业绩提成，剩余的收益金，本基金年化收益仍不低于 20%的情形下，圳湾金融服务有限公司按照本基金收益10%收取业绩提成</p>
+                        <p><span class="color1">业绩提成:</span>“<?= $model->name ?>”年化收益不低于20%的情形下，同信投资有限责任公司按照基金的 10%收取业绩提成，剩余的收益分配到本基金，本基金年化收益仍不低于 20%的情形下，深圳贝湾金融服务有限公司按照本基金收益10%收取业绩提成</p>
                     </div>
                 </li>
             </ul>
