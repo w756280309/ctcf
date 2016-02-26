4a2a2f5f525233813836755451064ddb80946b60
zhy9001 <zhanghongyu@wangcaigu.com>
2016-02-26 10:58:02 +0800

    recharge_cond

--- console/controllers/RechargeController.php
+++ console/controllers/RechargeController.php
@@ -12,8 +12,7 @@ class RechargeController extends Controller
     public function actionCheck()
     {
         // TODO 先查联动的记录
-        //$records = RechargeRecord::find()->where(['status' => RechargeRecord::STATUS_NO])->all();
-        $records = RechargeRecord::find()->where(['status' => RechargeRecord::STATUS_YES])->all();
+        $records = RechargeRecord::find()->where(['status' => RechargeRecord::STATUS_NO])->all();
         $acc_ser = new AccountService();
         foreach ($records as $rc) {
             $resp = Yii::$container->get('ump')->getRechargeInfo($rc->sn, $rc->created_at);
@@ -23,8 +22,7 @@ class RechargeController extends Controller
                 } else if ("3" === $resp->get('tran_state') || "5" === $resp->get('tran_state')) {
                     $rc->status = RechargeRecord::STATUS_FAULT;
                     $rc->save(false);
-                }
-                
+                }                
             }
         }
     }
