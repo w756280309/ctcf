49898a610d46aabe03009e205dabbd6a45814abf
Mo Di <modi@wangcaigu.com>
2016-03-01 14:02:34 +0800

    Changed DI to use prod certs.

--- common/config/di.php
+++ common/config/di.php
@@ -2,5 +2,5 @@
 
 \Yii::$container->set('account_service', 'common\\service\\AccountService');
 \Yii::$container->set('paginator', 'YiiPlus\\Paginator\\Paginator');
-\Yii::$container->set('ump', 'PayGate\\Ump\\Client', ['7050209', __DIR__.'/payment/ump/umpay.key', __DIR__.'/payment/ump/umpay.crt', \Yii::$app->params['ump']['notify']]);
+\Yii::$container->set('ump', 'PayGate\\Ump\\Client', ['7050209', __DIR__.'/payment/ump/wdjf_prod.key', __DIR__.'/payment/ump/ump_prod.crt', \Yii::$app->params['ump']['notify']]);
 \Yii::$container->set('sms', 'SmsGate\\SmsRequest', [true, ['15810036547', '18518154492']]);
