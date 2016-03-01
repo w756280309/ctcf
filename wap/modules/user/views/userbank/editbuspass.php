5980bddf557d5ee377faa8583d2fac3885a38e3f
zuoyuwei <zuoyuwei@wangcaigu.com>
2016-03-01 17:20:11 +0800

    修改交易密码页面

--- wap/modules/user/views/userbank/editbuspass.php
+++ wap/modules/user/views/userbank/editbuspass.php
@@ -6,13 +6,44 @@ $this->registerJsFile('/js/common.js', ['depends' => 'yii\web\YiiAsset','positio
 ?>
 <link rel="stylesheet" href="/css/base.css">
 <link rel="stylesheet" href="/css/setting.css">
-
-<?php $form = ActiveForm::begin(['id' => 'form', 'action' =>"/user/userbank/reset-trade-pass"]); ?>
-    <div>
-        <input id="editpassbtn" class="btn-common btn-normal" style="margin-top:40px;" type="button" value="确认重置">
-    </div>
-<?php ActiveForm::end(); ?>
-
+<style>
+	body{
+		background: #E8ECEB;
+	}
+	.container {
+		background: #E8ECEB;
+	}
+</style>
+	<div class="row" id='payment'>
+		<div class="col-xs-12">忘记资金托管账户支付密码</div>
+	</div>
+	<div class="row paymentLock">
+		<div class="col-xs-12">
+			<img src="/images/paymentLock.jpg" alt="">
+		</div>
+	</div>
+	<div class="row" id='payment-content'>
+		<div class="col-xs-12">若您忘记了您的资金托管账户支付密码，可以申请密码重置，联动优势将新的支付密码以短信形式发送到您的手机，请注意接收并妥善保管。</div>
+	</div>
+	<?php $form = ActiveForm::begin(['id' => 'form', 'action' =>"/user/userbank/reset-trade-pass"]); ?>
+	<div class="row">
+	    <div class="col-xs-12">
+	        <input id="editpassbtn" class="btn-common btn-normal" style="margin-top:10px;" type="button" value="重置支付密码">
+	    </div>
+	</div>
+	<?php ActiveForm::end(); ?>
+	<div class="row" id='payment-change'>
+		<div class="col-xs-12">修改资金托管账户支付密码</div>
+	</div>
+	<div class="row" id='payment-how'>
+		<div class="col-xs-12">您可以编辑短信<span>“GGMM#原密码#新密码”</span>(例如:GGMM#123456#234567) 发送至优势修改您的支付密码，支付密码只能是6位数字。</div>
+	</div>
+	<div class="row" id='payment-li'>
+		<div class="col-xs-12">
+			<div>联动优势短信号码:</div>
+			<div>移动，联通，电信用户编辑短信发至10690569687</div>
+		</div>
+	</div>
 <!-- 绑定提示 end  -->
 <!-- 修改登录密码页 end  -->
 <script type="text/javascript">
