3418a056ecb6b468d1c3372f31569073f21accb3
zhy9001 <zhanghongyu@wangcaigu.com>
2016-03-03 14:35:03 +0800

    增加提交绑卡验证卡归属行要求

--- wap/modules/user/controllers/qpay/BindingController.php
+++ wap/modules/user/controllers/qpay/BindingController.php
@@ -13,7 +13,6 @@ use common\models\bank\ConfigQpay;
 
 class BindingController extends BaseController
 {
-
     public function actionVerify()
     {
         Yii::$app->response->format = Response::FORMAT_JSON;
@@ -26,25 +25,27 @@ class BindingController extends BaseController
 
         $acct_model = new QpayAcct();
         $acct_model->uid = $this->user->id;
-        $acct_model->card_number = $safe['idNo'];
         $acct_model->account = $safe['realName'];
         $acct_model->account_type = QpayAcct::PERSONAL_ACCOUNT;
-        
-        try {
-            //对于绑卡时候如果没有找到要过滤掉异常
-            $bin = BankManager::getBankFromCardNo($acct_model->card_number);
-            if (!BankManager::isDebitCard($bin)) {
-                return $this->createErrorResponse('该操作只支持借记卡');
-            }    
-        } catch (\Exception $ex) {            
-        }
 
         if (
             $acct_model->load(Yii::$app->request->post())
             && $acct_model->validate()
         ) {
+            try {
+                //对于绑卡时候如果没有找到要过滤掉异常
+                $bin = BankManager::getBankFromCardNo($acct_model->card_number);
+                if (!BankManager::isDebitCard($bin)) {
+                    return $this->createErrorResponse('该操作只支持借记卡');
+                }
+                if ((int) $bin->bankId !== (int) $acct_model->bank_id) {
+                    return $this->createErrorResponse('请选择银行卡对应的银行');
+                }
+            } catch (\Exception $ex) {
+            }
+
             $qpay = ConfigQpay::findOne($acct_model->bank_id);
-            if (null === $qpay || 1 === (int)$qpay->isDisabled) {
+            if (null === $qpay || 1 === (int) $qpay->isDisabled) {
                 return $this->createErrorResponse('抱歉不支持当前选择的银行');
             }
             $acct_model->binding_sn = TxUtils::generateSn('B');
