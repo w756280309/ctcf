ffc21118852d4c9045efc448046446616e550784
zhy9001 <zhanghongyu@wangcaigu.com>
2016-03-07 17:36:43 +0800

    修改tradelog生成规则

--- common/models/Logger.php
+++ common/models/Logger.php
@@ -9,17 +9,26 @@ namespace common\models;
  */
 class Logger implements \PayGate\Ump\LoggerInterface
 {
-    
     /**
      * @param array             $rqData 不包含签名的内容，可以为null
      * @param array             $rq     签名后的数据，可以为null
      * @param ResponseInterface $rp
      * @param $duration 记录同步请求响应时间
+     * 说明:目前同步记录1开户；2建标；3标的更新；4标的转账；5 融资用户提现申请;6交易密码
      * return tradelog 对象
      */
     public function log($direction = 1, $rqData = null, $rq = null, $rp = null, $duration = 0)
     {
-        $log = TradeLog::initLog($direction, $rqData, $rq, $rp, $duration);
-        $log->save();
+        if (
+                'mer_register_person' === $rqData['service']
+                || 'mer_bind_project' === $rqData['service']
+                || 'mer_update_project' === $rqData['service']
+                || 'project_transfer' === $rqData['service']
+                || 'mer_withdrawals' === $rqData['service']
+                || 'mer_send_sms_pwd' === $rqData['service']
+         ) {
+            $log = TradeLog::initLog($direction, $rqData, $rq, $rp, $duration);
+            $log->save();
+        }
     }
 }
