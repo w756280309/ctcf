90ccf5b0eb98d4450d375e50971208cadc4a62c8
zuoyuwei <zuoyuwei@wangcaigu.com>
2016-03-09 16:17:50 +0800

    修正样式

--- common/models/sms/SmsTable.php
+++ common/models/sms/SmsTable.php
@@ -1,50 +1,37 @@
 <?php
+
 namespace common\models\sms;
 
 use yii\db\ActiveRecord;
 
-
-class SmsTable extends ActiveRecord {
-    
+class SmsTable extends ActiveRecord
+{
     const STATUS_USE = 1;
     const STATUS_UNUSE = 0;
-    
-//    const TYPE_REG_CODE = 1;
-//    const TYPE_FIND_PWD = 2;
-//    const TYPE_EXAMIN_PASS = 3;
-//    const TYPE_EDIT_BIND_MOBILE = 4;
-//    
-//    const SMS_CODE_REG = 12552;//PC端手机验证码注册用
-//    const SMS_CODE_EXAMIN_PASS = 16883;//  后台审核短信通知结果
-//    const SMS_CODE_EXAMIN_UNPASS = 16858;//后台审核短信通知结果 未通过
-//    const SMS_CODE_FIND_PWD = 16777;//PC端密码找回手机验证码 应该是17340
-//    const SMS_CODE_EDIT_BIND_MOBILE=17342;//端短信修改绑定手机
-//    
-//    const SMS_TIME_LEN_REG = 30;//注册码有效时间
-//    
-//    const SMS_STATUS_SUCCESS = '000000';
+
     /**
-     * @inheritdoc
+     * {@inheritdoc}
      */
-    public static function tableName() {
+    public static function tableName()
+    {
         return 'sms';
     }
-     
+
     /**
-     * @inheritdoc
+     * {@inheritdoc}
      */
     public function rules()
     {
         return [
-            [['type','mobile'], 'required'],
-            [['created_at'],'default',  'value' => time()],
-            [['status'],'default',  'value' => 0],
+            [['type', 'mobile'], 'required'],
+            [['created_at'], 'default',  'value' => time()],
+            [['status'], 'default',  'value' => 0],
             //['link_val','string']
         ];
     }
-    
+
     /**
-     * @inheritdoc
+     * {@inheritdoc}
      */
     public function attributeLabels()
     {
@@ -53,14 +40,14 @@ class SmsTable extends ActiveRecord {
             'code' => '短信码',
             'time_len' => '短信有效时长',
             'type' => '类型',
-            'uid'=>"uid",
-            'username'=>"username",
+            'uid' => 'uid',
+            'username' => 'username',
             'mobile' => '手机号',
             'end_time' => '截止日期',
             'created_at' => '创建时间',
         ];
     }
-    
+
     /*
      * 建议$cond 参数包含uid type mobile
      * $sms_model = new SmsTable();
@@ -82,5 +69,4 @@ class SmsTable extends ActiveRecord {
 //        }
 //        return array('result'=>1,'error'=>"",'obj_id'=>$sms->id);
 //    }
-    
 }
