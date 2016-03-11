fd722ef133734a10c05f9ce02d44e51bc50e506a
Mo Di <modi@wangcaigu.com>
2016-03-11 17:23:02 +0800

    补充遗漏的sql

--- db/160311_update_auth.sql
+++ db/160311_update_auth.sql
@@ -1,1 +1,2 @@
+ALTER TABLE `online_product` ADD COLUMN `recommendTime` int(10) DEFAULT NULL COMMENT '推荐时间' AFTER `is_xs`;
 insert into `auth` ( `auth_name`, `auth_description`, `created_at`, `psn`, `path`, `type`, `level`, `order_code`, `updated_at`, `sn`, `status`) values ( '推荐/取消推荐', '推荐/取消推荐', null, 'P200100', 'product/productonline/recommend', '2', '3', '2', null, 'P200113', '1');
