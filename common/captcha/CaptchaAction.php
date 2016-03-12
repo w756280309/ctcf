c2aa9afc0aa160d3dc33615771cd1144ca37f7bd
Mo Di <modi@wangcaigu.com>
2016-03-12 09:54:32 +0800

    使图片验证码随页面刷新而变化

--- common/captcha/CaptchaAction.php
+++ common/captcha/CaptchaAction.php
@@ -4,6 +4,9 @@ namespace common\captcha;
 
 use Gregwar\Captcha\CaptchaBuilder;
 use yii\captcha\CaptchaAction as BaseAction;
+use yii\helpers\Url;
+use yii\web\Response;
+use Yii;
 
 class CaptchaAction extends BaseAction
 {
@@ -12,5 +15,25 @@ class CaptchaAction extends BaseAction
         $builder = new CaptchaBuilder($code);
         $builder->build($this->width, $this->height);
         return $builder->get();
+    }
+
+    public function run()
+    {
+        if (Yii::$app->request->getQueryParam(self::REFRESH_GET_VAR) !== null) {
+            // AJAX request for regenerating code
+            $code = $this->getVerifyCode(true);
+            Yii::$app->response->format = Response::FORMAT_JSON;
+            return [
+                'hash1' => $this->generateValidationHash($code),
+                'hash2' => $this->generateValidationHash(strtolower($code)),
+                // we add a random 'v' parameter so that FireFox can refresh the image
+                // when src attribute of image tag is changed
+                'url' => Url::to([$this->id, 'v' => uniqid()]),
+            ];
+        } else {
+            $this->setHttpHeaders();
+            Yii::$app->response->format = Response::FORMAT_RAW;
+            return $this->renderImage($this->getVerifyCode(true));
+        }
     }
 }
