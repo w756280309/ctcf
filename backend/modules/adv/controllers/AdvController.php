113008bf9bf55bfdef6a5b9ac564f5acb3d58dd7
zuoyuwei <zuoyuwei@wangcaigu.com>
2016-03-18 11:00:48 +0800

    修改了ADV的sn生成规则

--- backend/modules/adv/controllers/AdvController.php
+++ backend/modules/adv/controllers/AdvController.php
@@ -14,56 +14,63 @@ use yii\web\Response;
  */
 class AdvController extends BaseController
 {
-     public function init() {
+    public function init()
+    {
         parent::init();
-        if (Yii::$app->request->isAjax)
+        if (Yii::$app->request->isAjax) {
             Yii::$app->response->format = Response::FORMAT_JSON;
+        }
     }
-    
+
     public function actionIndex()
     {
         //页面的搜索功能
         $status = Yii::$app->request->get('status');
         $title = Yii::$app->request->get('title');
-        
-        $advInfo = Adv::find()->select('id,sn,title,status,show_order')->andWhere(['del_status'=>  Adv::DEL_STATUS_SHOW]);
-        if(!empty($title)) {
-            $advInfo->andFilterWhere(['like',title,$title]);
+
+        $advInfo = Adv::find()->select('id,sn,title,status,show_order')->andWhere(['del_status' => Adv::DEL_STATUS_SHOW]);
+        if (!empty($title)) {
+            $advInfo->andFilterWhere(['like', title, $title]);
         }
-        if($status == 0 || $status == 1) {
+        if ($status == 0 || $status == 1) {
             $advInfo->andFilterWhere(['status' => $status]);
         }
         $data = $advInfo->orderBy('id desc');
         $pages = new Pagination(['totalCount' => $data->count(), 'pageSize' => '10']);
         $model = $data->offset($pages->offset)->limit($pages->limit)->orderBy('sn desc,status asc')->all();
-        return $this->render('index',['model'=>$model, 'pages' => $pages]);
+
+        return $this->render('index', ['model' => $model, 'pages' => $pages]);
     }
-    
-    public function actionEdit($id = null){
-        
+
+    /**
+     * 首页轮播图的添加
+     */
+    public function actionEdit($id = null)
+    {
         $model = $id ? Adv::findOne($id) : new Adv();
-        
-       
-        if($id){
+
+        if ($id) {
             $model->scenario = 'update';
-        }else{    
+        } else {
             $model->scenario = 'create';
             $model->creator_id = Yii::$app->user->id;
-            $model->sn = $createUsercode = Adv::create_code();
+            $model->sn = Adv::create_code();
         }
-        if ($model->load(Yii::$app->request->post())&& $model->validate())
-        { 
+        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
             $model->save();
             $this->alert = 1;
             $this->toUrl = 'index';
         }
-        return $this->render('edit',['model'=>$model]);
+
+        return $this->render('edit', ['model' => $model]);
     }
 
     /**
      * Deletes an existing OfflineOrder model.
      * If deletion is successful, the browser will be redirected to the 'index' page.
+     *
      * @param string $id
+     *
      * @return mixed
      */
     public function actionDelete($id)
@@ -71,15 +78,19 @@ class AdvController extends BaseController
         $model = $this->findModel($id);
         $model->scenario = 'update';
         $model->del_status = Adv::DEL_STATUS_DEL;
-        $result =  $model->save();
+        $result = $model->save();
+
         return $this->redirect('index');
     }
 
     /**
      * Finds the OfflineOrder model based on its primary key value.
      * If the model is not found, a 404 HTTP exception will be thrown.
+     *
      * @param string $id
+     *
      * @return OfflineOrder the loaded model
+     *
      * @throws NotFoundHttpException if the model cannot be found
      */
     protected function findModel($id)
@@ -90,64 +101,68 @@ class AdvController extends BaseController
             throw new NotFoundHttpException('The requested page does not exist.');
         }
     }
-    
-    
-    public function actionLineon(){
-        if(!Yii::$app->request->isPost){
+
+    public function actionLineon()
+    {
+        if (!Yii::$app->request->isPost) {
             return [
-                'result'=>0,
-                'message'=>'非法请求'
+                'result' => 0,
+                'message' => '非法请求',
             ];
         }
         $ids = Yii::$app->request->post('ids');
-        Adv::updateAll(['status'=>1], ['in', 'id', explode(',', $ids)]);
-        return ['result'=>1, 'message'=>'操作成功'];
+        Adv::updateAll(['status' => 1], ['in', 'id', explode(',', $ids)]);
+
+        return ['result' => 1, 'message' => '操作成功'];
     }
- 
-    public function actionLineoff(){
-        if(!Yii::$app->request->isPost){
+
+    public function actionLineoff()
+    {
+        if (!Yii::$app->request->isPost) {
             return [
-                'result'=>0,
-                'message'=>'非法请求'
+                'result' => 0,
+                'message' => '非法请求',
             ];
         }
         $ids = Yii::$app->request->post('ids');
-        Adv::updateAll(['status'=>0], ['in', 'id', explode(',', $ids)]);
-        return ['result'=>1, 'message'=>'操作成功'];
+        Adv::updateAll(['status' => 0], ['in', 'id', explode(',', $ids)]);
+
+        return ['result' => 1, 'message' => '操作成功'];
     }
-    
-    
-    public function actionMoreop($op=null,$id=null,$value=null) {
+
+    public function actionMoreop($op = null, $id = null, $value = null)
+    {
         $res = 0;
-        if($op=='status'){//项目状态
+        if ($op == 'status') {
+            //项目状态
             $_model = $this->findModel($id);
-            if($value==  Adv::STATUS_HIDDEN){
+            if ($value ==  Adv::STATUS_HIDDEN) {
                 $_model->status = Adv::STATUS_SHOW;
-            }else if($value==Adv::STATUS_SHOW){
+            } elseif ($value == Adv::STATUS_SHOW) {
                 $_model->status = Adv::STATUS_HIDDEN;
             }
             $_model->scenario = 'update';
             $res = $_model->save();
-        }else{
-            
+        } else {
         }
-        echo json_encode(array('res'=>$res));
+        echo json_encode(array('res' => $res));
     }
-    
-    //点击edit视图页面中的新添加的图片后，显示是否删除，删除需要删除掉服务器上刚上传的图片
-     public function actionImgdel($id = null, $img = null) {
-        if ($id) {
-            Adv::deleteAll(['id' => $id]);
-        }
-        $dr = $_SERVER['DOCUMENT_ROOT'];
-        
-        $f = $dr.'/upload/adv/'.$img;
-        if(file_exists($f)){
-            unlink($f);
-        }
-        echo json_encode(1);
-        exit;
-    }
-    
-    
+
+    /**
+     * 点击edit视图页面中的新添加的图片后，显示是否删除，删除需要删除掉服务器上刚上传的图片
+     */
+    public function actionImgdel($id = null, $img = null)
+    {
+         if ($id) {
+             Adv::deleteAll(['id' => $id]);
+         }
+         $dr = $_SERVER['DOCUMENT_ROOT'];
+
+         $f = $dr.'/upload/adv/'.$img;
+         if (file_exists($f)) {
+             unlink($f);
+         }
+         echo json_encode(1);
+         exit;
+     }
 }
--- common/models/adv/Adv.php
+++ common/models/adv/Adv.php
@@ -7,75 +7,77 @@ use yii\db\ActiveRecord;
 
 /**
  * This is the model class for table "adv_pos".
- *
  */
 class Adv extends ActiveRecord
 {
     //0显示，1隐藏
     const STATUS_SHOW = 0;
     const STATUS_HIDDEN = 1;
-    
+
     //0正常，1删除
     const DEL_STATUS_SHOW = 0;
     const DEL_STATUS_DEL = 1;
-    
-    const UPLOAD_PATH = "/upload/adv/";
 
+    const UPLOAD_PATH = '/upload/adv/';
 
-    public static function getStatusList(){
+    public static function getStatusList()
+    {
         return array(
             self::STATUS_SHOW => '显示',
-            self::STATUS_HIDDEN => '隐藏'
+            self::STATUS_HIDDEN => '隐藏',
         );
     }
-    
-    public static function getDelStatusList(){
+
+    public static function getDelStatusList()
+    {
         return array(
             self::DEL_STATUS_SHOW => '正常',
-            self::DEL_STATUS_DEL => '删除'
+            self::DEL_STATUS_DEL => '删除',
         );
     }
 
     /**
-     * @inheritdoc
+     * {@inheritdoc}
      */
     public static function tableName()
     {
         return 'adv';
     }
 
-    public function scenarios() {
-        return [
-            'update' => ['id','sn','title', 'pos_id', 'image', 'show_order', 'link','description','del_status'],
-            'create' => [ 'pos_id','sn','title', 'image', 'show_order', 'link','description','del_status'],
-        ];
-    }
-    
-    /**
-     * @inheritdoc
-     */
-    public function behaviors() {
-        return [
-            TimestampBehavior::className(),
-        ];
-    }
-    
-    /**
-     * @inheritdoc
-     */
-    public function rules()
+    public function scenarios()
     {
         return [
-            [[ 'image','description'], 'required', 'on' => ['create', 'update']],
-            ['status', 'default', 'value' => self::STATUS_SHOW, 'on' => ['create']],
-            ['del_status', 'default', 'value' => self::DEL_STATUS_SHOW, 'on' => ['create']],
-            [['link','link'],'match','pattern'=>'/^((\w\.)+)|[^\d]$/','message'=>'网址格式错误'],//一些简单的验证，不能为中文、不能为纯数字
-            
+            'update' => ['id', 'sn', 'title', 'pos_id', 'image', 'show_order', 'link', 'description', 'del_status'],
+            'create' => ['pos_id', 'sn', 'title', 'image', 'show_order', 'link', 'description', 'del_status'],
         ];
     }
 
     /**
-     * @inheritdoc
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            TimestampBehavior::className(),
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['image', 'description'], 'required', 'on' => ['create', 'update']],
+            ['status', 'default', 'value' => self::STATUS_SHOW, 'on' => ['create']],
+            ['del_status', 'default', 'value' => self::DEL_STATUS_SHOW, 'on' => ['create']],
+            [['link', 'link'], 'match', 'pattern' => '/^((\w\.)+)|[^\d]$/', 'message' => '网址格式错误'], //一些简单的验证，不能为中文、不能为纯数字
+
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
      */
     public function attributeLabels()
     {
@@ -92,39 +94,48 @@ class Adv extends ActiveRecord
             'created_at' => '添加时间',
         ];
     }
-    
-    public function getPosAdv($code=null){
-        $pos = AdvPos::findOne(['code'=> $code ,"del_status"=>  AdvPos::DEL_STATUS_SHOW]);
-        if(empty($pos)){
+
+    public function getPosAdv($code = null)
+    {
+        $pos = AdvPos::findOne(['code' => $code, 'del_status' => AdvPos::DEL_STATUS_SHOW]);
+        if (empty($pos)) {
             return array();
         }
-        $adv = self::find()->OrderBy(['id'=>SORT_DESC])->select(['image','link','description'])->andWhere(['pos_id'=>$pos->id,'status'=>  self::STATUS_SHOW,'del_status'=>  self::DEL_STATUS_SHOW])->limit($pos->number)->all();
+        $adv = self::find()->OrderBy(['id' => SORT_DESC])->select(['image', 'link', 'description'])->andWhere(['pos_id' => $pos->id, 'status' => self::STATUS_SHOW, 'del_status' => self::DEL_STATUS_SHOW])->limit($pos->number)->all();
         $adv_list = array();
-        foreach ($adv as $key=>$val){
-            $adv_list[$key]['image']=  self::UPLOAD_PATH.$val->image;
-            $adv_list[$key]['link']=$val->link;
-            $adv_list[$key]['description']=$val->description;
+        foreach ($adv as $key => $val) {
+            $adv_list[$key]['image'] = self::UPLOAD_PATH.$val->image;
+            $adv_list[$key]['link'] = $val->link;
+            $adv_list[$key]['description'] = $val->description;
         }
+
         return array(
-            "number"=>$pos->number,
-            "width"=>$pos->width,
-            "height"=>$pos->height,
-            "adv"=>$adv_list
+            'number' => $pos->number,
+            'width' => $pos->width,
+            'height' => $pos->height,
+            'adv' => $adv_list,
                 );
     }
-    
-     //sn
-    public static function create_code($pre="SYLB"){
+
+    /**
+     * 创建sn.
+     */
+    public static function create_code($pre = 'SYLB')
+    {
         $last = self::find()->select('sn')->orderBy('id desc')->one();
         $sn = $pre;
-        if(strpos($last['sn'], '0000')===FALSE){//没有编号的
-            $sn .= '0000';
-        }else{
+        if (!$last || empty($last['sn'])) {
+            //没有编号的
+            $sn .= '0001';
+        } else {
             $step = \Yii::$app->functions->autoInc(substr($last['sn'], 4, 4));
+            if (9999 < (int) $step) {
+                $step = '0001';
+            }
+
             $sn .= $step;
         }
-        return $sn;  
-    }
 
-    
+        return $sn;
+    }
 }
