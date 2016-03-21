e12abcacbf3ec5a295c380a27ddc89d10e6bb3fc
zuoyuwei <zuoyuwei@wangcaigu.com>
2016-03-21 17:16:00 +0800

    增加引入函数

--- wap/modules/user/views/user/mingxi.php
+++ wap/modules/user/views/user/mingxi.php
@@ -6,7 +6,7 @@ foreach (Yii::$app->params['mingxi'] as $key => $val) {
     $this->registerJs('mingxitype['.$key.'] = \''.$val.'\';', 1);
 }
 $this->registerJsFile(ASSETS_BASE_URI . 'js/moment.min.js', ['depends' => 'yii\web\YiiAsset','position' => 1]);
-$this->registerJsFile(ASSETS_BASE_URI . 'js/jiaoyimingxi.js', ['depends' => 'yii\web\YiiAsset','position' => 1]);
+$this->registerJsFile(ASSETS_BASE_URI . 'js/jiaoyimingxi.js?v=20160321', ['depends' => 'yii\web\YiiAsset','position' => 1]);
 frontend\assets\WapAsset::register($this);
 $this->registerJs('var total=' . $header['count'] . ';', 1);
 $this->registerJs('var size=' . $header['size'] . ';', 1);
@@ -14,7 +14,7 @@ $this->registerJs('var tp=' . $header['tp'] . ';', 1);
 $this->registerJs('var cp=' . $header['cp'] . ';', 1);
 ?>
 <link rel="stylesheet" href="<?= ASSETS_BASE_URI ?>css/base.css"/>
-<link rel="stylesheet" href="<?= ASSETS_BASE_URI ?>css/jiaoyimingxi.css"/>
+<link rel="stylesheet" href="<?= ASSETS_BASE_URI ?>css/jiaoyimingxi.css?v=20160321"/>
 <div class="container">
     <?php if ($model) {
     foreach ($model as $val): ?>
