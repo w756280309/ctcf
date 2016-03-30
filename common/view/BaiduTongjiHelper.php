06e419c016cd403a55f04327c4eb9813ce71e7cf
Mo Di <modi@wangcaigu.com>
2016-03-30 18:09:18 +0800

    百度统计合并站点KEY

--- common/view/BaiduTongjiHelper.php
+++ common/view/BaiduTongjiHelper.php
@@ -7,7 +7,8 @@ use yii\web\View;
 class BaiduTongjiHelper
 {
     const WAP_KEY = 'd2417f8d221ffd4b883d5e257e21736c';
-    const PC_KEY = '22888bd72b3ee39b96705e3ce4492484';
+    const PC_KEY = 'd2417f8d221ffd4b883d5e257e21736c'; // 合并PC站统计
+    //const PC_KEY = '22888bd72b3ee39b96705e3ce4492484';
 
     public static function registerTo($viewObj, $key)
     {
