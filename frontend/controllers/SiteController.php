393c305ed5b4dd85c7ea89037fa9d54213430f80
zuoyuwei <zuoyuwei@wangcaigu.com>
2016-04-06 16:39:34 +0800

    删除多余的代码

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -29,13 +29,8 @@ class SiteController extends Controller
         return [
             'access' => [
                 'class' => AccessControl::className(),
-                'only' => ['logout', 'signup'],
+                'only' => ['logout'],
                 'rules' => [
-                    [
-                        'actions' => ['signup'],
-                        'allow' => true,
-                        'roles' => ['?'],
-                    ],
                     [
                         'actions' => ['logout'],
                         'allow' => true,
