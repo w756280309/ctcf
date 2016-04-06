d3e752d8791a77311544ec2b6ceaaf90a8eb2a6b
zuoyuwei <zuoyuwei@wangcaigu.com>
2016-04-06 16:47:31 +0800

    修复样式

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -8,7 +8,7 @@ $this->title = '温都金服首页';
         <div class="first-box-left">
             <div class="first-box-left1">
                 <div class="first-title">
-                    <p><img src="<?=ASSETS_BASE_URI ?>images/he.png" alt=""></p>
+                    <p><img src="<?= ASSETS_BASE_URI ?>images/he.png" alt=""></p>
                     <div class="first-title-content">
                         <div>温州报业传媒</div>
                         <div>旗下理财平台</div>
@@ -22,8 +22,8 @@ $this->title = '温都金服首页';
             </div>
         </div>
         <div class="first-box-right">
-            <img src="<?=ASSETS_BASE_URI ?>images/phone.png" class="first-phone1" alt="">
-            <img src="<?=ASSETS_BASE_URI ?>images/phone1.png" class="first-phone2" alt="">
+            <img src="<?= ASSETS_BASE_URI ?>images/phone.png" class="first-phone1" alt="">
+            <img src="<?= ASSETS_BASE_URI ?>images/phone1.png" class="first-phone2" alt="">
         </div>
     </div>
 </div>
@@ -34,7 +34,7 @@ $this->title = '温都金服首页';
         <div class="tow-content2">全场加息0.1%</div>
         <div class="tow-content3">iphone6s</div>
         <div class="tow-content4">iphone6s</div>
-        <img src="<?=ASSETS_BASE_URI ?>images/phone2.png" class="tow-phone" alt="">
+        <img src="<?= ASSETS_BASE_URI ?>images/phone2.png" class="tow-phone" alt="">
     </div>
 </div>
 <!--section3-->
@@ -43,37 +43,37 @@ $this->title = '温都金服首页';
         <div class="three-box-title">国资平台 值得信任</div>
         <div class="three-box-title1">—— 温都金服五大优势 ——</div>
         <div class="three-content three-content1">
-            <img src="<?=ASSETS_BASE_URI ?>images/one.png" alt="">
+            <img src="<?= ASSETS_BASE_URI ?>images/one.png" alt="">
             <div class="three-content-title"><span>国资</span>背景</div>
             <i>隶属温州日报报业集团旗下，国资平台，可信赖；</i>
         </div>
         <div class="three-content three-content2">
-            <img src="<?=ASSETS_BASE_URI ?>images/two.png" alt="">
+            <img src="<?= ASSETS_BASE_URI ?>images/two.png" alt="">
             <div class="three-content-title"><span>股东</span>强势</div>
             <em><p>温州日报报业集团，实力雄厚的现代国有传媒集团；</p><p>南京金融资产交易中心，省级金融主管部门审批的合规平台；</p></em>
         </div>
         <div class="three-content three-content3">
-            <img src="<?=ASSETS_BASE_URI ?>images/three.png" alt="">
+            <img src="<?= ASSETS_BASE_URI ?>images/three.png" alt="">
             <div class="three-content-title"><span>安全</span>合规</div>
             <em><p>资金全程托管，与平台隔离不被挪用；绑定本人银行卡，提现只进本人账户很安全；</p><p>层层风控，安全至上；专业稳健，合法合规；</p></em>
         </div>
         <div class="three-content three-content4">
-            <img src="<?=ASSETS_BASE_URI ?>images/four.png" alt="">
+            <img src="<?= ASSETS_BASE_URI ?>images/four.png" alt="">
             <div class="three-content-title">产品<span>优质</span></div>
             <em><p>主流金融机构产品、优质政府平台类产品、</p><p>优质供应链金融产品，期限多样，收益可观；</p></em>
         </div>
         <div class="three-content three-content5">
-            <img src="<?=ASSETS_BASE_URI ?>images/five.png" alt="">
+            <img src="<?= ASSETS_BASE_URI ?>images/five.png" alt="">
             <div class="three-content-title"><span>灵活</span>便捷</div>
             <em><p>网上有平台，线下有门店，温都金服在您身边，</p><p>千元起投，手机操作，随时随地实现财富增值。</p></em>
         </div>
         <div class="three-hang">
-            <img src="<?=ASSETS_BASE_URI ?>images/gongsi.png" alt="">
+            <img src="<?= ASSETS_BASE_URI ?>images/gongsi.png" alt="">
             <p>*联动优势是中国银联与中国移动联合成立的第三方支付公司</p>
         </div>
     </div>
     <div class="three-bottom">
-        <img src="<?=ASSETS_BASE_URI ?>images/three-footer.png" alt="">
+        <img src="<?= ASSETS_BASE_URI ?>images/three-footer.png" alt="">
     </div>
 </div>
 <!--section4-->
@@ -83,14 +83,14 @@ $this->title = '温都金服首页';
         <div class="four-content"><span>专业、安全、规范、</span>最有诚意的国资互联网金融平台</div>
         <div class="four-ins">
             <div class="four-left">
-                <img src="<?=ASSETS_BASE_URI ?>images/four-left.png" alt="">
+                <img src="<?= ASSETS_BASE_URI ?>images/four-left.png" alt="">
                 <div class="four-left-content">温州温都金融信息服务股份有限公司（简称“温都金服”），隶属温州报业传媒旗下，核心股东为温州报业传媒有限公司和南京金融资产交易中心，是由双方分别整合自身强大的公信力、受众、行业资源优势和专业的风控、理财产品采集、网络技术能力所倾力打造的一家理财平台。<br/><br/>温都金服将以客户需求为中心，运用互联网技术提供金融及产业服务的创新模式，通过规范的操作流程和完善的风险控制体系，为符合国家发展战略的产业和企业提供多元化的平台，为客户供应更契合自身资产配置的金融产品，为广大用户创造更多的价值，助推普惠金融的发展。</div>
             </div>
             <div class="four-right">
-                <img src="<?=ASSETS_BASE_URI ?>images/four-right.png" alt="">
+                <img src="<?= ASSETS_BASE_URI ?>images/four-right.png" alt="">
             </div>
             <div class="four-bottom">
-                <img src="<?=ASSETS_BASE_URI ?>images/four-bottom.png" alt="">
+                <img src="<?= ASSETS_BASE_URI ?>images/four-bottom.png" alt="">
             </div>
         </div>
         <div style="clear: both"></div>
