2fd7ab82394bc3ba0ae83c5ff8ff78319d387a43
zhy9001 <zhanghongyu@wangcaigu.com>
2016-04-07 19:48:33 +0800

    增加ajax提交增加token变量

--- wap/views/layouts/main.php
+++ wap/views/layouts/main.php
@@ -19,6 +19,15 @@ BaiduTongjiHelper::registerTo($this, BaiduTongjiHelper::WAP_KEY);
 
     <script src="<?= ASSETS_BASE_URI ?>js/jquery.js"></script>
     <script src="<?= ASSETS_BASE_URI ?>js/common.js?v=160407"></script>
+    <script>
+$(document).ajaxSend(function(event, jqXHR, settings) {
+    var match = window.location.search.match(new RegExp('[?&]token=([^&]+)(&|$)'));
+    if (match) {
+        var val = decodeURIComponent(match[1].replace(/\+/g, " "));
+        settings.url = settings.url+(settings.url.indexOf('?') >= 0 ? '&' : '?')+'token='+encodeURIComponent(val);
+    }
+});
+</script>
 </head>
 <body>
 <?php $this->beginBody() ?>
