2a72a9453172c4589a327e981991f087d0ea1b3d
Mo Di <modi@wangcaigu.com>
2016-04-15 19:46:15 +0800

    修改上线日期

--- wap/views/news/detail.php
+++ wap/views/news/detail.php
@@ -7,17 +7,17 @@ $this->title = '最新资讯';
 <!-- 主体 -->
 <div class="information-details">
         <div class="header">
-                <p class="header-title">温都金服定于4月6日试上线</p>
-                <p class="header-time">2016-4-5</p>
+                <p class="header-title">温都金服定于4月19日试上线</p>
+                <p class="header-time">2016-4-18</p>
         </div>
         <div class="header-content">
                 <p class="text-indent-no">亲爱的朋友：</p>
-                <p>经过长久的精心筹备，温州报业传媒旗下理财平台——温都金服（wenjf.com），定于4月6日试上线。</p>
+                <p>经过长久的精心筹备，温州报业传媒旗下理财平台——温都金服（wenjf.com），定于4月19日试上线。</p>
                 <p>上线当日，温都金服将发布精选优质资产项目，数量有限、秒杀在即！如需快速抢标，请您提前注册、充值。</p>
-                <p>4月6日起至4月20日，平台将推出“全场加息、投资前8名送iPhone6S、iPad mini4等奖品，理财用户人人抽奖、注册绑卡用户凭注册信息可到门店领取精美礼品等系列活动”，欢迎关注！</p>
+                <p>4月19日起至5月3日，平台将推出“全场加息、投资前8名送iPhone6S、iPad mini4等奖品，理财用户人人抽奖、注册绑卡用户凭注册信息可到门店领取精美礼品等系列活动”，欢迎关注！</p>
                 <p>活动请点击首页相应页面查看、参与！</p>
                 <p class="header-content-luokuan">温都金服运营团队</p>
-                <p class="header-content-time">2016年4月5日</p>
+                <p class="header-content-time">2016年4月18日</p>
         </div>
 </div>
 <?php } elseif (2 === (int) $id) { ?>
@@ -25,7 +25,7 @@ $this->title = '最新资讯';
 <div class="information-details">
         <div class="header">
                 <p class="header-title">用户资金托管引入联动优势</p>
-                <p class="header-time">2016-4-5</p>
+                <p class="header-time">2016-4-18</p>
         </div>
         <div class="header-content">
                 <p class="text-inline">温都金服（wenjf.com）与联动优势科技有限公司（由中国银联与中国移动联合发起成立）签订合作协议，采用联动优势资金托管综合服务，对平台的交易施行资金托管与独立结算，用户资金只存在于联动优势支付平台的托管账户中，这将充分保障用户资金与温都金服平台的分离。</p>
@@ -33,7 +33,7 @@ $this->title = '最新资讯';
                 <p>联动优势科技有限公司由中国移动与中国银联于2003年8月联合发起成立，致力于为企业和消费者提供创新、绿色、安全、高效的金融及电子商务产品和服务， 是目前国内领先的跨综合支付服务、智能金融信息服务、移动化的本地多应用服务三大领域的移动金融及移动电子商务产业链服务提供商。</p>
                 <p> 联动优势拥有历经数年考验的专业风控体系，采用国际最先进的动态密码算法，最大限度保护账户及资金安全；使用国际先进的客户关系管理系统和数据挖掘系统， 加强对交易的实时监控；2010年，系统已达到国家信息安全检测中心的系统安全一级资质，是中国商务信用平台，并通过ISO27001安全认证。</p>
                 <p class="header-content-luokuan">温都金服运营团队</p>
-                <p class="header-content-time">2016年4月5日</p>
+                <p class="header-content-time">2016年4月18日</p>
         </div>
 </div>
 <?php } ?>
--- wap/views/news/index.php
+++ wap/views/news/index.php
@@ -6,14 +6,14 @@ $this->title = '最新资讯';
 <!-- 主体 -->
 <div class="information-list">
         <a href="/news/detail?id=1" class="single">
-                <p class="single-title">【资讯标题】温都金服定于4月6日试上线</p>
-                <p class="single-description">温州报业传媒旗下理财平台—温都金服（wenjf.com），定于4月6日试上线。</p>
-                <p class="single-time">2016-4-5</p>
+                <p class="single-title">【资讯标题】温都金服定于4月19日试上线</p>
+                <p class="single-description">温州报业传媒旗下理财平台—温都金服（wenjf.com），定于4月19日试上线。</p>
+                <p class="single-time">2016-4-18</p>
         </a>
         <a href="/news/detail?id=2" class="single">
                 <p class="single-title">【资讯标题】用户资金托管引入联动优势</p>
                 <p class="single-description">温都金服引入联动优势（由中国银联与中国移动联合发起成立）提供用户资金托管综合服务。</p>
-                <p class="single-time">2016-4-5</p>
+                <p class="single-time">2016-4-18</p>
         </a>
         <!--加载跟多-->
         <div class="load" style="display:block;"></div>
