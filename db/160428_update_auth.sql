4bc7ce46629ffeb1f90537817d297fbb82b3d1ac
Mo Di <modi@wangcaigu.com>
2016-04-28 13:36:36 +0800

    补充“数据统计”的权限记录

--- db/160428_update_auth.sql
+++ db/160428_update_auth.sql
@@ -1,1 +1,2 @@
+insert into `auth` ( `auth_name`, `auth_description`, `created_at`, `psn`, `path`, `type`, `level`, `order_code`, `updated_at`, `sn`, `status`) values ( '数据统计', '数据统计模块', null, '0', 'datatj/datatj/huizongtj', '1', '1', '5', null, 'D100000', '1');
 insert into `auth` ( `auth_name`, `auth_description`, `created_at`, `psn`, `path`, `type`, `level`, `order_code`, `updated_at`, `sn`, `status`) values ( '汇总统计', '汇总统计', null, 'D100000', 'datatj/datatj/huizongtj', '1', '2', '5', null, 'D100100', '1');
