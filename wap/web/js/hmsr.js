2c545853c06b8acc39274c3774bc2f7d84ebb863
shiyang <shiyang@wangcaigu.com>
2016-05-03 13:19:58 +0800

    ﻿来源统计：取消跨站cookie功能
    Summary: 来源统计：取消跨站cookie功能
    
    Test Plan: 来源统计：取消跨站cookie功能
    
    Reviewers: modi
    
    Differential Revision: http://dx.wangcaigu.cn/D196

--- wap/web/js/hmsr.js
+++ wap/web/js/hmsr.js
@@ -1,9 +1,7 @@
 $(function () {
-    var domain = document.domain;
-    var new_domain = domain.replace(/((\w+\.)*)(\w+(\.\w+)?)/, ".$2$3");
     var r = new RegExp("(^|&)" + 'hmsr' + "=([^&]*)(&|$)");
     var result = window.location.search.substr(1).match(r);
     if (result && result[2]) {
-        var res = $.cookie('campaign_source', result[2], {expires: 3, domain: new_domain});
+        var res = $.cookie('campaign_source', result[2], {expires: 3});
     }
 });
