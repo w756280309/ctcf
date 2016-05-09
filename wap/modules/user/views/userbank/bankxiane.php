e7cfd5aab282a2c11f974ee06e411d48e3279355
zuoyuwei <zuoyuwei@wangcaigu.com>
2016-05-09 10:55:09 +0800

    去掉银行卡限额页面下面的页尾导航条
    Summary: 修改银行限额页面样式
    
    Test Plan: 去掉银行卡限额页面下面的页尾导航条
    
    Reviewers: zuoyuwei
    
    Differential Revision: http://dx.wangcaigu.cn/D227

--- wap/modules/user/views/userbank/bankxiane.php
+++ wap/modules/user/views/userbank/bankxiane.php
@@ -1,10 +1,7 @@
 <?php
-    $this->title="限额说明";
+    $this->title = '限额说明';
 ?>
  <style>
-    body {
-        padding-bottom: 50px;
-    }
     h1 {
         font-size: 16px;
         color: #595757;
@@ -33,6 +30,7 @@
         margin: 0 auto;
     }
  </style>
+
  <div class="qpay-quota">
     <h1>各银行限额说明<font style="size: 30px; color:#898989">（仅限储蓄卡）</h1>
     <table>
@@ -41,9 +39,13 @@
             <th width="33%">单笔</th>
             <th>单日</th>
         </tr>
-        <?php foreach($banks as $bank): ?>
+        <?php foreach ($banks as $bank): ?>
         <tr>
-            <td><?= $bank->bank->bankName ?><?php if ($bank->isDisabled) { echo "(暂停)";} ?></td>
+            <td><?= $bank->bank->bankName ?>
+                <?php if ($bank->isDisabled) {
+                    echo '(暂停)';
+                } ?>
+            </td>
             <td><?= \Yii::$app->functions->toFormatMoney($bank->singleLimit) ?></td>
             <td><?= \Yii::$app->functions->toFormatMoney($bank->dailyLimit) ?></td>
         </tr>
@@ -53,30 +55,6 @@
     <ol style="font-size: 14px; color: #737373;padding-bottom:30px;">
         <li class="desc">根据同卡进出原则，用户只能使用唯一一张绑定的银行卡进行充值和提现</li>
         <li class="desc">暂不支持变更绑定银行卡，如需帮助，请联系客服</li>
-        <li style="text-align: right; color: #f44639"><img src="" alt=""><?= Yii::$app->params['contact_tel'] ?></li>
+        <li style="text-align: right; color: #f44639"><img src="" alt=""><?= \Yii::$app->params['contact_tel'] ?></li>
     </ol>
  </div>
- <!--footer-->
-<?php if (!defined('IN_APP')) { ?>
-<div class="row navbar-fixed-bottom footer">
-    <div class="col-xs-4 footer-title">
-        <div class="footer-inner">
-            <a href="/" class="shouye1"><span class="shouye"></span>首页</a>
-        </div>
-    </div>
-    <div class="col-xs-4 footer-title">
-        <div class="footer-inner1">
-            <a href="/deal/deal/index"><span class="licai"></span>理财</a>
-        </div>
-    </div>
-    <div class="col-xs-4 footer-title">
-        <div class="footer-inner2">
-            <?php if (!\Yii::$app->user->isGuest) { ?>
-            <a href="/user/user"><span class="zhanghu"></span>账户</a>
-            <?php } else { ?>
-            <a href="/site/login"><span class="zhanghu"></span>账户</a>
-            <?php } ?>
-        </div>
-    </div>
-</div>
- <?php } ?>
