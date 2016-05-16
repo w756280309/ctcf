3f1e27c3fbf2d38ab8124a1acbb9051843fb109f
zuoyuwei <zuoyuwei@wangcaigu.com>
2016-05-16 09:15:14 +0800

    修改页面文案
    Summary: 修改页面文案
    
    Test Plan: 修改页面文案
    
    Reviewers: zuoyuwei
    
    Differential Revision: http://dx.wangcaigu.cn/D278

--- wap/modules/order/themes/dahe/booking/booking.php
+++ wap/modules/order/themes/dahe/booking/booking.php
@@ -29,7 +29,7 @@ $this->title = '预约申请单';
     </div>
     <div class="row">
         <div class="hidden-xs col-sm-1"></div>
-        <div class="col-xs-7 col-sm-8"><p class="p-notice">*温股投起购金额<?= $product->min_fund ?>万元</p></div>
+        <div class="col-xs-7 col-sm-8"><p class="p-notice">*股权项目起购金额<?= $product->min_fund ?>万元</p></div>
         <div class="hidden-xs col-sm-1"></div>
     </div>
     <!--提交按钮-->
