671504c31ebc1873f625cefa681a7aa599144049
Mo Di <modi@wangcaigu.com>
2016-05-18 10:31:23 +0800

    online order的user coupon id不检查唯一

--- console/migrations/m160517_182811_alter_online_order.php
+++ console/migrations/m160517_182811_alter_online_order.php
@@ -6,7 +6,7 @@ class m160517_182811_alter_online_order extends Migration
 {
     public function up()
     {
-        $this->addColumn('online_order', 'userCoupon_id', $this->integer()->unique());//用户coupon中的id
+        $this->addColumn('online_order', 'userCoupon_id', $this->integer());//用户coupon中的id
         $this->addColumn('online_order', 'couponAmount', $this->decimal(6, 2)->notNull());//代金券金额
         $this->addColumn('online_order', 'paymentAmount', $this->decimal(14, 2)->notNull());//实付金额
     }
