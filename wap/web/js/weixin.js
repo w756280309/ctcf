ad1724e7a77018ee6d4fe014d82c8984c0b5a23b
Mo Di <modi@wangcaigu.com>
2016-05-19 14:27:26 +0800

    hotfix：微信分享

--- wap/web/js/weixin.js
+++ wap/web/js/weixin.js
@@ -7,7 +7,7 @@ xhr.done(function(data) {
         timestamp: data.timestamp, // 必填，生成签名的时间戳
         nonceStr: data.nonceStr, // 必填，生成签名的随机串
         signature: data.signature,// 必填，签名，见附录1
-        jsApiList: [] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+        jsApiList: ['onMenuShareAppMessage'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
     });
 
     wx.ready(function(){
