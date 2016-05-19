9a824e42290cec58c633ed97d7e035259c65b58c
shiyang <shiyang@wangcaigu.com>
2016-05-19 19:40:51 +0800

    ﻿大河报wap版微调
    Summary: 大河报wap版微调
    
    Test Plan: 大河报wap版微调
    
    Reviewers: shiyang
    
    Differential Revision: http://dx.wangcaigu.cn/D318

--- wap/themes/dahe/site/contact.php
+++ wap/themes/dahe/site/contact.php
@@ -10,6 +10,7 @@ $this->registerCssFile(ASSETS_BASE_URI . 'css/contact.css');
         <div class="xinxi"><p class="p_float">公司地址&nbsp;:</p><p class="p_num">河南省郑州市农业路东28号报业大厦1507室</p></div>
         <div class="xinxi"><p class="p_float">工作时间&nbsp;:</p><p class="p_num">8:30-12:00,14:00-17:00（周一至周五）</p></div>
         <div class="xinxi"><p class="p_float">咨询电话&nbsp;:</p><p class="p_num">0371-65795606、65795631</p></div>
+        <div class="xinxi"><p class="p_float">会员QQ群&nbsp;:</p><p class="p_num">439311692、60231370</p></div>
         <div class="xinxi"><p class="p_float">门店地址&nbsp;:</p></div>
         <div class="mendian"><p class="p_num">1、城东路服务部：郑州市城东路顺河路口南20米路东，大河报广告中心营业部</p></div>
         <div class="xinxi"><p class="p_num">2、大学路服务部：郑州市中原路与大学路交会处向北50米路东，大河广告中心营业部</p></div>
--- wap/themes/dahe/site/index.php
+++ wap/themes/dahe/site/index.php
@@ -60,7 +60,7 @@ $(function() {
     <!--header-->
     <header class="row head-title">
         <div class="logo col-xs-12 col-sm-12"><img src="<?= ASSETS_BASE_URI ?>images/dahe/logo.png?v=20160519" alt="logo" ></div>
-        <div class="logo_tit">河南日报报业集团旗下理财平台</div>
+        <div class="logo_tit">大河报旗下理财平台</div>
     </header>
     <?php } ?>
 
