d651262496b976f842890aa15f577ebe40b98434
shiyang <shiyang@wangcaigu.com>
2016-05-30 10:52:11 +0800

    ﻿加载指定视图模板文件
    Summary: 加载指定视图模板文件
    
    Test Plan: 加载指定视图模板文件
    
    Reviewers: modi
    
    Differential Revision: http://dx.wangcaigu.cn/D377

---
+++ wap/controllers/DevController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace app\controllers;
+
+
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use Yii;
+
+class DevController extends Controller
+{
+    public function actionView($viewPath)
+    {
+        if ('dev' !== YII_ENV) {
+            throw new NotFoundHttpException('只有在 ENV 环境下允许访问该页面。请通过index-dev.php入口文件进行访问');
+        }
+        $basePath = Yii::getAlias('@wap') . '/';
+        $viewFile = $basePath . $viewPath;
+        if (file_exists($viewFile) && is_file($viewFile)) {
+            $ext = pathinfo($viewFile, PATHINFO_EXTENSION);
+            if ('php' !== $ext) {
+                throw new NotFoundHttpException('模板文件只能是php文件（扩展名位.php）');
+            }
+            return $this->renderFile($viewFile);
+        } else {
+            throw new NotFoundHttpException('指定模板（' . $viewFile . '）没有找到。模板示例：文件位置，wap/views/ebaoquan/index.php;$viewPath = "views/ebaoquan/index.php";访问地址 /index-dev.php/dev/view?viewPath=views/ebaoquan/index.php ');
+        }
+    }
+}
