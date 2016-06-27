957e18d4470532d62a664048e261772b329fd65d
liufengjun <liufengjun@wangcaigu.com>
2016-06-27 16:34:58 +0800

    操作结果页样式bug
    Summary: 修改操作结果页样式bug
    
    Test Plan: 查看样式，所有失败页按钮取消，参数错误抛404
    
    Reviewers: modi
    
    Subscribers: liufengjun
    
    Differential Revision: http://dx.wangcaigu.cn/D545

--- frontend/controllers/InfoController.php
+++ frontend/controllers/InfoController.php
@@ -2,15 +2,16 @@
 namespace frontend\controllers;
 
 use yii\web\Controller;
+use yii\web\NotFoundHttpException;
 
 class InfoController extends Controller
 {
     private $config = [
         'success' => [
             'tuoguan' => [
-                'title' => '结果',
+                'title' => '开通资金托管结果',
                 'mainTitle' => '恭喜您，资金托管账户开通成功！',
-                'firstFuTitle' => '<em class="em">支付密码将会以短信的形式发送到您的手机上</em>，请注意查收并妥善保存。<br>支付密码为6位随机数，可根据短信内容修改密码。',
+                'firstFuTitle' => '<em style="color:#f44336;">支付密码将会以短信的形式发送到您的手机上</em>，请注意查收并妥善保存。<br>支付密码为6位随机数，可根据短信内容修改密码。',
                 'requireJump' => true,
                 'linkType' => 1,
                 'jumpReferWords' => '下一步',
@@ -58,12 +59,12 @@ class InfoController extends Controller
         ],
         'fail' => [
             'tuoguan' => [
-                'title' => '结果',
+                'title' => '开通资金托管结果',
                 'mainTitle' => '资金托管账户开通失败！',
                 'firstFuTitle' => '如有疑问，请联系客服，电话：400-101-5151(09:00-20:00)',
                 'requireJump' => true,
-                'linkType' => 1,
-                'jumpReferWords' => '重新开通',
+                'linkType' => 0,
+                'jumpReferWords' => '',
             ],
             'touzi' => [
                 'title' => '购买结果',
@@ -94,8 +95,8 @@ class InfoController extends Controller
                 'mainTitle' => '充值失败！',
                 'firstFuTitle' => '如有疑问，请联系客服，电话：400-101-5151(09:00-20:00)',
                 'requireJump' => true,
-                'linkType' => 1,
-                'jumpReferWords' => '重新充值',
+                'linkType' => 0,
+                'jumpReferWords' => '',
             ],
             'tixian' => [
                 'title' => '提现结果',
@@ -141,7 +142,7 @@ class InfoController extends Controller
     private function paramValid($source)
     {
         if (!in_array($source, $this->sources)) {
-            return $this->goHome();
+            throw new NotFoundHttpException();
         }
     }
 }
