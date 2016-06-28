0e347fae0a3c6f681e342044b1180ea516b49973
liufengjun <liufengjun@wangcaigu.com>
2016-06-28 18:17:22 +0800

    全站右侧帮助小工具
    Summary: 全站右侧帮助小工具
    
    Test Plan: 测试小工具的样式，正确性与是否覆盖全站
    
    Reviewers: liufengjun
    
    Subscribers: liufengjun
    
    Differential Revision: http://dx.wangcaigu.cn/D553

--- frontend/views/404.php
+++ frontend/views/404.php
@@ -6,6 +6,8 @@
     <link rel="stylesheet" href="<?= ASSETS_BASE_URI ?>css/base.css">
     <link rel="stylesheet" href="<?= ASSETS_BASE_URI ?>css/404.css">
     <link rel="stylesheet" href="<?= ASSETS_BASE_URI ?>css/footer.css">
+    <script src="<?= ASSETS_BASE_URI ?>js/jquery-1.8.3.min.js"></script>
+    <script src="<?= ASSETS_BASE_URI ?>js/clipboard.min.js"></script>
 </head>
 <body>
 <div class="container">
@@ -32,31 +34,6 @@
         </div>
     </div>
 </div>
-<div class="footer-section footer-section5 footer-fp-auto-height footer-fp-section footer-fp-table">
-    <div class="footer-five-box" style="height: 200px">
-        <div class="footer-five-address">公司地址：温州市鹿城区飞霞南路657号保丰大楼四层</div>
-        <div class="footer-five-tel">客服电话：<span><?= Yii::$app->params['contact_tel'] ?></span><span style="padding-left: 8px;margin-right: 8px;">客服QQ：1430843929</span>客服时间：9:00-20:00（周一至周日）</div>
-        <div class="footer-five-partner footer-clearfix"><i>合作伙伴：</i>
-            <ul>
-                <li style="border-left: 0;padding-left: 0;">温州日报</li>
-                <li>温州商报</li>
-                <li>温州都市报</li>
-                <li>温州晚报</li>
-                <li>科技金融时报</li>
-                <li>温州网</li>
-                <li>温州人杂志</li>
-                <li style="border-left:0;padding-left: 0;">南京金融资产交易中心</li>
-                <li>同信证券</li>
-                <li>联动优势</li>
-                <li>电子数据保全中心</li>
-            </ul>
-        </div>
-        <div class="footer-five-copyright">Copyright ©温州温都金融信息服务股份有限公司 浙ICP备16003187号-1</div>
-        <div class="footer-first-ma">
-            <img src="<?= ASSETS_BASE_URI ?>images/ma.png" alt="">
-            <div>访问手机wap版</div>
-        </div>
-    </div>
-</div>
+<?= $this->render("@frontend/views/footer.php") ?>
 </body>
 </html>
