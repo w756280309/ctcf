a82542ccd691fcaacf04bcaf890091849e23fa24
liufengjun <liufengjun@wangcaigu.com>
2016-06-28 16:06:08 +0800

    样式调整
    Summary: 404样式调整
    
    Test Plan: 查看样式
    
    Reviewers: modi
    
    Subscribers: liufengjun
    
    Differential Revision: http://dx.wangcaigu.cn/D550

--- frontend/views/404.php
+++ frontend/views/404.php
@@ -3,16 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <title>404</title>
-    <link rel="stylesheet" href="../css/base.css">
-    <link rel="stylesheet" href="../css/404.css">
-    <link rel="stylesheet" href="../css/footer.css">
+    <link rel="stylesheet" href="<?= ASSETS_BASE_URI ?>css/base.css">
+    <link rel="stylesheet" href="<?= ASSETS_BASE_URI ?>css/404.css">
+    <link rel="stylesheet" href="<?= ASSETS_BASE_URI ?>css/footer.css">
 </head>
 <body>
 <div class="container">
     <div class="header-nav-box">
         <a href="/">
             <div class="header-logo">
-                <img src="../images/404/404-logo.png" alt="logo">
+                <img src="<?= ASSETS_BASE_URI ?>images/404/404-logo.png" alt="logo">
             </div>
         </a>
     </div>
@@ -22,10 +22,10 @@
             <p class="error">抱歉！当前正有大批理财者进入，您暂时无法访问此页面！<a href="/" class="back-home">回首页></a></p>
             <p class="code-tip">如果您急需我们，请扫描下方二维码。</p>
             <ul class="ul-code">
-                <li><a href="javascript:void(0)"><img src="../images/404/404-wdjg-code.png" alt="微信订阅号"></a>
+                <li><a href="javascript:void(0)"><img src="<?= ASSETS_BASE_URI ?>images/404/404-wdjg-code.png" alt="微信订阅号"></a>
                     <p>微信订阅号(项目预告)</p>
                 </li>
-                <li><a href="javascript:void(0)"><img src="../images/404/404-wdjg-codeapp.png" alt="温都金服app"></a>
+                <li><a href="javascript:void(0)"><img src="<?= ASSETS_BASE_URI ?>images/404/404-wdjg-codeapp.png" alt="温都金服app"></a>
                     <p>温都金服APP</p>
                 </li>
             </ul>
@@ -53,7 +53,7 @@
         </div>
         <div class="footer-five-copyright">Copyright ©温州温都金融信息服务股份有限公司 浙ICP备16003187号-1</div>
         <div class="footer-first-ma">
-            <img src="../images/ma.png" alt="">
+            <img src="<?= ASSETS_BASE_URI ?>images/ma.png" alt="">
             <div>访问手机wap版</div>
         </div>
     </div>
