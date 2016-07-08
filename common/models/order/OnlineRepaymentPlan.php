f9d2624ac14430d9ad23ab087f48d5bdc998ce61
shiyang <shiyang@wangcaigu.com>
2016-07-08 10:19:40 +0800

    ﻿项目期限计算：有截止日期时候，期限=截止日期-起息日期+1
    Summary: 项目期限计算：有截止日期时候，期限=截止日期-起息日期+1
    
    Test Plan: 项目期限计算：有截止日期时候，期限=截止日期-起息日期+1
    
    Reviewers: modi
    
    Differential Revision: http://dx.wangcaigu.cn/D611

--- common/models/order/OnlineRepaymentPlan.php
+++ common/models/order/OnlineRepaymentPlan.php
@@ -272,12 +272,15 @@ class OnlineRepaymentPlan extends \yii\db\ActiveRecord
                 $up['finish_date'] = strtotime($finish_date);
                 $loan->finish_date = $up['finish_date'];
             }
+        } else {
+            //有截止日期时候，项目期限=截止日期 - 起息日期 + 1
+            if (OnlineProduct::REFUND_METHOD_DAOQIBENXI === (int) $loan->refund_method) {
+                $expires = (new \DateTime(date('Y-m-d', $loan->finish_date)))->diff((new \DateTime(date('Y-m-d',$loan->jixi_time))))->days + 1;
+                $loan->expires = $expires;
+                $up['expires'] = $expires;
+            }
         }
-        if (OnlineProduct::REFUND_METHOD_DAOQIBENXI === (int) $loan->refund_method) {
-            $expires = (new \DateTime(date('Y-m-d', $loan->finish_date)))->diff((new \DateTime(date('Y-m-d',$loan->jixi_time))))->days + 1;
-            $loan->expires = $expires;
-            $up['expires'] = $expires;
-        }
+
         OnlineProduct::updateAll($up, ['id' => $loan->id]);//修改已经计息
         $username = '';
         $sms = new SmsMessage([
