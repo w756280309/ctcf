ddf9f4e4f488fc9b99ff56ee7704e414fe31f440
shiyang <shiyang@wangcaigu.com>
2016-07-20 13:28:17 +0800

    ﻿还款：新建数据库、添加模型
    Summary: 还款：新建数据库、添加模型
    
    Test Plan: 还款：新建数据库、添加模型
    
    Reviewers: modi
    
    Differential Revision: http://dx.wangcaigu.cn/D705

---
+++ common/models/payment/Repayment.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace common\models\payment;
+
+use Yii;
+use yii\db\ActiveRecord;
+
+/**
+ * This is the model class for table "repayment".
+ *
+ * @property integer $id
+ * @property integer $loan_id   标的ID
+ * @property integer $term  分期期数
+ * @property string $dueDate    预期还款时间
+ * @property string $amount    应还总金额
+ * @property string $principal  本金
+ * @property string $interest   利息
+ * @property integer $isRepaid  是否还款，指融资用户扣钱
+ * @property string $repaidAt  还款时间 dateTime类型
+ * @property integer $isRefund  是否回款,指给用户转钱
+ * @property string $refundedAt 回款时间 dateTime类型
+ */
+class Repayment extends ActiveRecord
+{
+    public static function tableName()
+    {
+        return 'repayment';
+    }
+
+    public function rules()
+    {
+        return [
+            [['loan_id', 'term', 'isRepaid', 'isRefund'], 'integer'],
+            [['dueDate', 'repaidAt', 'refundedAt'], 'safe'],
+            [['amount', 'principal', 'interest'], 'number']
+        ];
+    }
+
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'loan_id' => '标的ID',
+            'term' => '分期期数',
+            'dueDate' => '预期还款时间',
+            'amount' => '应还总金额',
+            'principal' => '本金',
+            'interest' => '利息',
+            'isRepaid' => '是否还款',
+            'repaidAt' => '还款时间',
+            'isRefund' => '是否回款',
+            'refundedAt' => '回款时间',
+        ];
+    }
+}
