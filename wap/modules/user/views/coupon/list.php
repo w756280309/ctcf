9276396344048ebf7b8503721189870934e3eb5f
Mo Di <modi@wangcaigu.com>
2016-08-01 15:52:55 +0800

    hotfix: css version

--- wap/modules/user/views/coupon/list.php
+++ wap/modules/user/views/coupon/list.php
@@ -1,7 +1,7 @@
 <?php
 $this->title = '我的代金券';
 
-$this->registerCssFile(ASSETS_BASE_URI . 'css/coupon.css?v=20150520', ['depends' => 'wap\assets\WapAsset']);
+$this->registerCssFile(ASSETS_BASE_URI . 'css/coupon.css?v=20150801', ['depends' => 'wap\assets\WapAsset']);
 $this->registerJsFile(ASSETS_BASE_URI . 'js/coupon-list.js?v=20150708', ['depends' => 'yii\web\YiiAsset', 'position' => 1]);
 $this->registerJsFile(ASSETS_BASE_URI . 'js/couponcode.js', ['depends' => 'wap\assets\WapAsset']);
 $this->registerJs('var tp=' . $header->pageCount . ';', 1);
