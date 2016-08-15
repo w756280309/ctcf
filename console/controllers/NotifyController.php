0ee76aeaef4e41f33db7af98785e31fe98a51823
shiyang <shiyang@wangcaigu.com>
2016-08-15 15:52:15 +0800

    ﻿钉钉提醒过滤测试标
    Summary:
    钉钉提醒过滤测试标
    
    更新钉钉
    
    Test Plan: 钉钉提醒过滤测试标
    
    Reviewers: modi
    
    Differential Revision: http://dx.wangcaigu.cn/D841

--- console/controllers/NotifyController.php
+++ console/controllers/NotifyController.php
@@ -18,7 +18,7 @@ class NotifyController extends Controller
         $new_query = clone $query;
         //查询当天
         $query->where(['<', 'dueDate', date('Y-m-d', strtotime("+1 days"))]);//所有区段都要统计自截止日之前的所有待还款项目
-        $res1 = $query->andWhere(['isRefunded' => 0, "$op.status" => OnlineProduct::STATUS_HUAN])->select(['loan_id'])->distinct()->count();
+        $res1 = $query->andWhere(['isRefunded' => 0, "$op.status" => OnlineProduct::STATUS_HUAN, "$op.isTest" => 0])->select(['loan_id'])->distinct()->count();
         //查询7天
         $new_query->where(['<', 'dueDate', date('Y-m-d', strtotime("+7 days"))]);//所有区段都要统计自截止日之前的所有待还款项目
         $res2 = $new_query->andWhere(['isRefunded' => 0, "$op.status" => OnlineProduct::STATUS_HUAN, "$op.isTest" => 0])->select(['loan_id'])->distinct()->count();
