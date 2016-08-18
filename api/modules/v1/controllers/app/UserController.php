6d821c5769510a3f1fe58afd07f03ca0f5b629eb
zuoyuwei <zuoyuwei@wangcaigu.com>
2016-08-18 09:12:06 +0800

    为APP用户接口新增换卡标志位
    Summary: 为APP用户接口新增换卡标志位
    
    Test Plan: 为APP用户接口新增换卡标志位
    
    Reviewers: modi
    
    Differential Revision: http://dx.wangcaigu.cn/D857

--- api/modules/v1/controllers/app/UserController.php
+++ api/modules/v1/controllers/app/UserController.php
@@ -4,21 +4,22 @@ namespace api\modules\v1\controllers\app;
 
 use api\modules\v1\controllers\Controller;
 use common\models\app\AccessToken;
+use common\models\bank\BankCardUpdate;
 
 /**
- * App相关api接口
+ * App相关api接口.
  */
 class UserController extends Controller
 {
     /**
-     * 获取用户信息
+     * 获取用户信息.
      */
     public function actionInfo($token)
     {
         if (empty($token)) {
             return [
-                'status' => "fail",//程序级别成功失败
-                'message' => "需要登录",
+                'status' => 'fail', //程序级别成功失败
+                'message' => '需要登录',
                 'data' => null,
             ];
         }
@@ -27,8 +28,8 @@ class UserController extends Controller
 
         if (null === $accessToken) {
             return [
-                'status' => "fail",//程序级别成功失败
-                'message' => "需要登录",
+                'status' => 'fail', //程序级别成功失败
+                'message' => '需要登录',
                 'data' => null,
             ];
         }
@@ -36,15 +37,25 @@ class UserController extends Controller
         $user = $accessToken->user;
         if (!$user) {
             return [
-                'status' => "fail",//程序级别成功失败
-                'message' => "找不到用户",
+                'status' => 'fail', //程序级别成功失败
+                'message' => '找不到用户',
                 'data' => null,
             ];
         }
 
         $bank = $user->qpay;
+        $cardUpdateFlag = false;
+
         if (null !== $bank) {
-            $bankCard = substr_replace($bank->card_number,'*****',3,-2);
+            $bankCard = substr_replace($bank->card_number, '*****', 3, -2);
+
+            $bankcardUpdate = BankCardUpdate::find()
+                ->where(['oldSn' => $bank->binding_sn, 'uid' => $user->id])
+                ->orderBy('id desc')->one();
+
+            if ($bankcardUpdate && BankCardUpdate::STATUS_ACCEPT === $bankcardUpdate->status) {
+                $cardUpdateFlag = true;
+            }
         } else {
             $bankCard = null;
         }
@@ -52,17 +63,17 @@ class UserController extends Controller
         $ua = $user->lendAccount;
         if (null === $ua) {
             return [
-                'status' => "fail",//程序级别成功失败
-                'message' => "找不到账户",
+                'status' => 'fail', //程序级别成功失败
+                'message' => '找不到账户',
                 'data' => null,
             ];
         }
 
         return [
-            'status' => 'success',//程序级别成功失败
+            'status' => 'success', //程序级别成功失败
             'message' => '成功',
             'data' => [
-                'result' => 'success',//业务级别成功失败
+                'result' => 'success', //业务级别成功失败
                 'msg' => '成功',
                 'content' => [
                     'user_id' => $user->id,
@@ -70,11 +81,12 @@ class UserController extends Controller
                     'profit_balance' => strval($ua->getTotalProfit()),
                     'investment_balance' => strval(bcadd($ua->investment_balance, $ua->freeze_balance, 2)),
                     'available_balance' => strval($ua->available_balance),
-                    'mobile' => substr_replace($user->mobile,'***', 3, -4),
-                    'idcard' => empty($user->idcard) ? null : substr_replace($user->idcard,'***', 5, -2),
+                    'mobile' => substr_replace($user->mobile, '***', 3, -4),
+                    'idcard' => empty($user->idcard) ? null : substr_replace($user->idcard, '***', 5, -2),
                     'bankcard' => $bankCard,
-                ]
-            ]
+                    'bankcard_update_pending' => $cardUpdateFlag,
+                ],
+            ],
         ];
     }
 }
