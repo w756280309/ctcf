bb165baaa5781eb3d283c96195c566442790535b
zuoyuwei <zuoyuwei@wangcaigu.com>
2016-09-18 17:05:07 +0800

    修改版本更新API接口
    Summary: 修改版本更新API接口
    
    Test Plan: 修改版本更新API接口
    
    Reviewers: modi
    
    Differential Revision: http://dx.wangcaigu.cn/D1022

--- api/modules/v1/controllers/app/DeployController.php
+++ api/modules/v1/controllers/app/DeployController.php
@@ -14,6 +14,7 @@ class DeployController extends Controller
      */
     public function actionAppver($version, $clienttype)
     {
+        $versionCode = (int) $version;
         $clienttype = strtolower($clienttype);
 
         if (empty($version) || !in_array($clienttype, ['ios', 'android'])) {
@@ -24,19 +25,31 @@ class DeployController extends Controller
             ];
         }
 
+        if ($versionCode >= 2) {
+            $content = [
+                'old_updatetype' => 1,
+                'updatedesc' => null,  //新版本更新说明
+                'versioncode' => null,   //新版本版本号
+                'versionname' => null, //新版本版本名称
+                'downloadurl' => null,  //新版本下载地址
+            ];
+        } else {
+            $content = [
+                'old_updatetype' => 2,  //旧版本更新类型
+                'updatedesc' => '增加用户换卡功能，增加邀请好友功能，修复bug',  //新版本更新说明
+                'versioncode' => '2',   //新版本版本号
+                'versionname' => '1.1', //新版本版本名称
+                'downloadurl' => ('ios' === $clienttype) ? 'https://itunes.apple.com/us/app/wen-dou-jin-fu/id1107540109?mt=8' : 'http://dapp.wenjf.com/wjf_v1.1.apk',   //新版本下载地址
+            ];
+        }
+
         return [
             'status' => 'success', //程序级别成功失败
             'message' => '成功',
             'data' => [
                 'result' => 'success', //业务级别成功失败
                 'msg' => '成功',
-                'content' => [
-                    'old_updatetype' => 2,  //旧版本更新类型
-                    'updatedesc' => '增加用户换卡功能，增加邀请好友功能，修复bug',  //新版本更新说明
-                    'versioncode' => '2',   //新版本版本号
-                    'versionname' => '1.1', //新版本版本名称
-                    'downloadurl' => ('ios' === $clienttype) ? 'https://itunes.apple.com/us/app/wen-dou-jin-fu/id1107540109?mt=8' : 'http://dapp.wenjf.com/wjf_v1.1.apk',   //新版本下载地址
-                ],
+                'content' => $content,
             ],
         ];
     }
