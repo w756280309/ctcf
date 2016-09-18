93bc5952950aa53ec08a307042fd04a072367930
zuoyuwei <zuoyuwei@wangcaigu.com>
2016-09-18 16:11:41 +0800

    更新版本更新接口
    Summary: 更新版本更新接口
    
    Test Plan: 更新版本更新接口
    
    Differential Revision: http://dx.wangcaigu.cn/D1021

--- api/modules/v1/controllers/app/DeployController.php
+++ api/modules/v1/controllers/app/DeployController.php
@@ -5,29 +5,39 @@ namespace api\modules\v1\controllers\app;
 use api\modules\v1\controllers\Controller;
 
 /**
- * App相关api接口
+ * App相关api接口.
  */
 class DeployController extends Controller
 {
     /**
-     * 版本更新信息
+     * 版本更新信息.
      */
-    public function actionAppver($version)
+    public function actionAppver($version, $clienttype)
     {
+        $clienttype = strtolower($clienttype);
+
+        if (empty($version) || !in_array($clienttype, ['ios', 'android'])) {
+            return [
+                'status' => 'fail', //程序级别成功失败
+                'message' => '参数错误',
+                'data' => null,
+            ];
+        }
+
         return [
-            'status' => 'success',//程序级别成功失败
+            'status' => 'success', //程序级别成功失败
             'message' => '成功',
             'data' => [
-                'result' => 'success',//业务级别成功失败
+                'result' => 'success', //业务级别成功失败
                 'msg' => '成功',
                 'content' => [
-                    'old_updatetype' => 1, //不更新
-                    'updatedesc' => null,
-                    'versioncode' => null,
-                    'versionname' => null,
-                    'downloadurl' => null,
-                ]
-            ]
+                    'old_updatetype' => 2,  //旧版本更新类型
+                    'updatedesc' => '增加用户换卡功能，增加邀请好友功能，修复bug',  //新版本更新说明
+                    'versioncode' => '2',   //新版本版本号
+                    'versionname' => '1.1', //新版本版本名称
+                    'downloadurl' => ('ios' === $clienttype) ? 'https://itunes.apple.com/us/app/wen-dou-jin-fu/id1107540109?mt=8' : 'http://dapp.wenjf.com/wjf_v1.1.apk',   //新版本下载地址
+                ],
+            ],
         ];
     }
 }
