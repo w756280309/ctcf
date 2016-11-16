fdc24d9d31f99b21b2ae38ff14037b3996bf1c71
zuoyuwei <zuoyuwei@wangcaigu.com>
2016-11-16 12:59:53 +0800

    WDJF-PROD-PC端活动页增加按钮
    Summary:
    1. PC端活动页增加注册，立即投资两个按钮；
    2. PC端活动页增加百度谷歌统计；
    
    Test Plan: 查看相关页面按钮是否显示正确
    
    Reviewers: modi
    
    Differential Revision: http://dx.wangcaigu.cn/D1375

---
+++ frontend/web/js/analytics.js
@@ -0,0 +1,33 @@
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "//hm.baidu.com/hm.js?d2417f8d221ffd4b883d5e257e21736c";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
+
+(function(i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r]=i[r] || function() {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+    a = s.createElement(o), m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+ga('create', 'UA-77716435-1', '.wenjf.com');
+ga('send', 'pageview');
+
+window._pt_lt = new Date().getTime();
+window._pt_sp_2 = [];
+_pt_sp_2.push('setAccount,6d0b2c48');
+var _protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+(function() {
+    var atag = document.createElement('script'); atag.type = 'text/javascript'; atag.async = true;
+    atag.src = _protocol + 'js.ptengine.cn/pta.js';
+    var stag = document.createElement('script'); stag.type = 'text/javascript'; stag.async = true;
+    stag.src = _protocol + 'js.ptengine.cn/pts.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(atag, s); s.parentNode.insertBefore(stag, s);
+})();
