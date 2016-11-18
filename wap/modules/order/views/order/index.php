ec027fa8fcff9c4e97fa889f7a3e0138a18c2649
zuoyuwei <zuoyuwei@wangcaigu.com>
2016-11-18 12:40:46 +0800

    WDJF-PROD-程序错误-删除普通标的购买页面H5弹框提示
    Summary: 1. 删除普通标的购买页面H5弹框提示；
    
    Test Plan: 1. 删除普通标的购买页面H5弹框提示；
    
    Reviewers: modi
    
    Differential Revision: http://dx.wangcaigu.cn/D1404

--- wap/modules/order/views/order/index.php
+++ wap/modules/order/views/order/index.php
@@ -32,18 +32,18 @@ $this->registerJsFile(ASSETS_BASE_URI.'js/order.js?v=20160908', ['depends' => 'y
             <?php $ex = $deal->getDuration() ?><?= $ex['value'] ?><?= $ex['unit'] ?>
             <?php if (!empty($deal->kuanxianqi)) { ?>(含宽限期<?=$deal->kuanxianqi?>天)<?php } ?></div>
         <div class="col-xs-4 text-align-ct">可投余额</div>
-        <div class="col-xs-8 text-align-lf col"><?=  number_format($param['order_balance'], 2)?>元</div>
+        <div class="col-xs-8 text-align-lf col"><?= StringUtils::amountFormat3($param['order_balance']) ?>元</div>
     </div>
     <div class="row surplus margin-top">
         <div class="col-xs-4 text-align-ct">可用金额</div>
-        <div class="col-xs-5 safe-lf text-align-lf"><?=  number_format($param['my_balance'], 2)?>元</div>
+        <div class="col-xs-5 safe-lf text-align-lf"><?= StringUtils::amountFormat3($param['my_balance']) ?>元</div>
         <div class="col-xs-3 safe-txt text-align-ct"><a href="/user/userbank/recharge?from=<?= urlencode('/order/order?sn='.$deal->sn)?>">去充值</a></div>
     </div>
     <form action="/order/order/doorder?sn=<?= $deal->sn ?>" method="post" id="orderform" data-to="1">
         <input name="_csrf" type="hidden" id="_csrf" value="<?=Yii::$app->request->csrfToken ?>">
         <div class="row sm-height border-bottom">
             <div class="col-xs-4 safe-txt text-align-ct">投资金额</div>
-            <input name="money" type="number" id="money" value="<?= empty($money) ? '' : $money ?>" placeholder="请输入投资金额"  class="col-xs-6 safe-lf text-align-lf" step="<?=$deal->dizeng_money ?>">
+            <input name="money" type="number" id="money" value="<?= empty($money) ? '' : $money ?>" placeholder="请输入投资金额"  class="col-xs-6 safe-lf text-align-lf" step="any">
             <div class="col-xs-2 safe-txt">元</div>
         </div>
 
