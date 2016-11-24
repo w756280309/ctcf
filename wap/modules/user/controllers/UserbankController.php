eb609b644c921cb9bf6d832eb0c4e02b3ec65828
liufengjun <liufengjun@wangcaigu.com>
2016-11-24 14:39:17 +0800

    WDJF-PROD-提现手续费还原为收取交易方手续费2元
    Summary: 提现手续费还原为收取交易方手续费2元
    
    Test Plan: 不再根据提现次数限制来判断收取交易方还是温都的手续费
    
    Reviewers: modi
    
    Subscribers: liufengjun
    
    Differential Revision: http://dx.wangcaigu.cn/D1440

--- wap/modules/user/controllers/UserbankController.php
+++ wap/modules/user/controllers/UserbankController.php
@@ -184,8 +184,7 @@ class UserbankController extends BaseController
         $draw->uid = $uid;
         if ($draw->load(Yii::$app->request->post()) && $draw->validate()) {
             try {
-                $drawFee = $user->getDrawCount() >= Yii::$app->params['draw_free_limit'] ? Yii::$app->params['drawFee'] : 0;
-                $drawres = DrawManager::initDraw($user_acount, $draw->money, $drawFee);
+                $drawres = DrawManager::initDraw($user_acount, $draw->money, Yii::$app->params['drawFee']);
                 $option = array();
                 if (null != Yii::$app->request->get('token')) {
                     $option['app_token'] = Yii::$app->request->get('token');
