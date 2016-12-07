77df1fa6e2131338abaf7c3f7d7a5f9e4c5d9559
liangwei02 <liangwei02@wangcaigu.com>
2016-12-07 19:05:43 +0800

    双十二活动

---
+++ fe/src/wap/campaigns/double-twelve/js/index.js
@@ -0,0 +1,40 @@
+/**
+ * Created by lw on 2016/12/7.
+ */
+$(function(){
+    FastClick.attach(document.body);
+    var len =$('.people ul li').length;
+    if(len > 3){
+        lunBo();
+    }
+    $('.btest').on('click',function () {
+        $('.mask').show();
+        var classNmae = $(this).data('value');
+        $('.'+classNmae).show();
+        document.body.addEventListener('touchmove',eventTarget, false);
+    })
+
+    document.getElementById("giftBox").addEventListener("touchmove",function(e){
+        e.stopPropagation();
+        //return false;
+    },false);
+    $('.mask,.closepop').on('click',function () {
+        $('.pop').hide();
+        $('.mask').hide();
+        document.body.removeEventListener('touchmove',eventTarget, false);
+    });
+})
+
+function lunBo(){
+    var liH = 0.693;
+    var ts = setInterval(function(){
+        $('.people ul li').eq(0).animate({"margin-top":"-"+liH+"rem"},1000,function(){
+            $(this).css({"margin-top":0});
+            $(this).appendTo($('.people ul'));
+        });
+    },2000);
+}
+
+function eventTarget(event){
+    event.preventDefault();
+}
