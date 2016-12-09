3b95aca73cbeec342a85a9518e2f6a5acfbf3fbb
shiyang <shiyang@wangcaigu.com>
2016-12-09 14:57:38 +0800

    邀请好友送抽奖机会判断逻辑修改
    Summary:
    问题描述：原判断逻辑中将count()结果转整形的逻辑被删除
    方案：数据库count()后转整形
    
    Test Plan: 邀请好友
    
    Reviewers: modi
    
    Differential Revision: http://dx.wangcaigu.cn/D1502

--- common/models/promo/Promo1212.php
+++ common/models/promo/Promo1212.php
@@ -361,8 +361,8 @@ class Promo1212
     private function addInitTicketInternal(User $user, Request $request = null)
     {
         //获取用户初始化的抽奖机会
-        $ticketCount = PromoLotteryTicket::find()->where(['user_id' => $user->id, 'source' => 'init', 'promo_id' => $this->promo->id])->count();
-        if (!$ticketCount) {
+        $ticketCount = (int)PromoLotteryTicket::find()->where(['user_id' => $user->id, 'source' => 'init', 'promo_id' => $this->promo->id])->count();
+        if ($ticketCount === 0) {
             $this->addTicketInternal($user->id, 'init', $this->promo->id, empty($request) ? '' : $request->getUserIP());
         }
     }
@@ -379,9 +379,9 @@ class Promo1212
         if (!empty($inviteRecord)) {
             $inviterId = $inviteRecord->user_id;
             //获取邀请者在活动期间邀请人数
-            $inviteCount = InviteRecord::find()->where(['user_id' => $inviterId])->andWhere(['>=', 'created_at', $this->promo->startAt])->count();
+            $inviteCount = (int)InviteRecord::find()->where(['user_id' => $inviterId])->andWhere(['>=', 'created_at', $this->promo->startAt])->count();
             //获取当前用户因为邀请被赠送的抽奖机会
-            $ticketCount = PromoLotteryTicket::find()->where(['user_id' => $inviterId, 'source' => 'invite', 'promo_id' => $this->promo->id])->count();
+            $ticketCount = (int)PromoLotteryTicket::find()->where(['user_id' => $inviterId, 'source' => 'invite', 'promo_id' => $this->promo->id])->count();
             //用户第一次邀请，给一次抽奖机会
             if ($inviteCount === 1 && $ticketCount === 0) {
                 $this->addTicketInternal($inviterId, 'invite', $this->promo->id, empty($request) ? '' : $request->getUserIP());
