55a97cad5cae789bd2e846fe4a9a64a494b90656
zuoyuwei <zuoyuwei@wangcaigu.com>
2017-01-03 13:29:09 +0800

    WDJF-BUG修改-购买页面标题过长显示全部
    Summary: 1. 购买页面标题过长显示全部；
    
    Test Plan: 查看WAP端购买页面
    
    Reviewers: modi
    
    Differential Revision: http://dx.wangcaigu.cn/D1687

--- wap/modules/order/views/order/index.php
+++ wap/modules/order/views/order/index.php
@@ -1,8 +1,11 @@
 <?php
-$this->title="购买";
+
+$this->title = '购买';
 
 use common\utils\StringUtils;
 use common\view\LoanHelper;
+use wap\assets\WapAsset;
+use yii\web\YiiAsset;
 
 $yr = $deal->yield_rate;
 $qixian = $deal->getDuration()['value'];
@@ -18,100 +21,101 @@ $this->registerJs(<<<JS
 JS
     , 1);
 
-$this->registerJsFile(ASSETS_BASE_URI.'js/order.js?v=20160908', ['depends' => 'yii\web\YiiAsset']);
+$this->registerJsFile(ASSETS_BASE_URI.'js/order.js?v=20160908', ['depends' => YiiAsset::class]);
+$this->registerCssFile(ASSETS_BASE_URI.'css/setting.css?v=20170103', ['depends' => WapAsset::class]);
+
 ?>
-<link rel="stylesheet" href="<?= ASSETS_BASE_URI ?>css/setting.css?v=20161226">
 
 <!--   购买页 start-->
-    <div class="row produce">
-        <div class="col-xs-12 text-align-lf first-hang" style="padding-right: 0;"><?=$deal->title?></div>
-        <div class="col-xs-4 text-align-ct">预期年化收益</div>
-        <div class="col-xs-8 text-align-lf col"><?= LoanHelper::getDealRate($deal) ?>%<?php if (!empty($deal->jiaxi)) { ?>+<?= $deal->jiaxi ?>%<?php } ?></div>
-        <div class="col-xs-4 text-align-ct">项目</div>
-        <div class="col-xs-8 text-align-lf col">
-            <?php $ex = $deal->getDuration() ?><?= $ex['value'] ?><?= $ex['unit'] ?>
-            <?php if (!empty($deal->kuanxianqi)) { ?>(含宽限期<?=$deal->kuanxianqi?>天)<?php } ?></div>
-        <div class="col-xs-4 text-align-ct">可投余额</div>
-        <div class="col-xs-8 text-align-lf col"><?= StringUtils::amountFormat3($param['order_balance']) ?>元</div>
+<div class="row produce">
+    <div class="col-xs-12 text-align-lf first-hang"><?=$deal->title?></div>
+    <div class="col-xs-4 text-align-ct">预期年化收益</div>
+    <div class="col-xs-8 text-align-lf col"><?= LoanHelper::getDealRate($deal) ?>%<?php if (!empty($deal->jiaxi)) { ?>+<?= $deal->jiaxi ?>%<?php } ?></div>
+    <div class="col-xs-4 text-align-ct">项目</div>
+    <div class="col-xs-8 text-align-lf col">
+        <?php $ex = $deal->getDuration() ?><?= $ex['value'] ?><?= $ex['unit'] ?>
+        <?php if (!empty($deal->kuanxianqi)) { ?>(含宽限期<?=$deal->kuanxianqi?>天)<?php } ?></div>
+    <div class="col-xs-4 text-align-ct">可投余额</div>
+    <div class="col-xs-8 text-align-lf col"><?= StringUtils::amountFormat3($param['order_balance']) ?>元</div>
+</div>
+<div class="row surplus margin-top">
+    <div class="col-xs-4 text-align-ct">可用金额</div>
+    <div class="col-xs-5 safe-lf text-align-lf"><?= StringUtils::amountFormat3($param['my_balance']) ?>元</div>
+    <div class="col-xs-3 safe-txt text-align-ct"><a href="/user/userbank/recharge?from=<?= urlencode('/order/order?sn='.$deal->sn)?>">去充值</a></div>
+</div>
+<form action="/order/order/doorder?sn=<?= $deal->sn ?>" method="post" id="orderform" data-to="1">
+    <input name="_csrf" type="hidden" id="_csrf" value="<?=Yii::$app->request->csrfToken ?>">
+    <div class="row sm-height border-bottom">
+        <div class="col-xs-4 safe-txt text-align-ct">投资金额</div>
+        <input name="money" type="number" id="money" value="<?= empty($money) ? '' : $money ?>" placeholder="请输入投资金额"  class="col-xs-6 safe-lf text-align-lf" step="any">
+        <div class="col-xs-2 safe-txt">元</div>
     </div>
-    <div class="row surplus margin-top">
-        <div class="col-xs-4 text-align-ct">可用金额</div>
-        <div class="col-xs-5 safe-lf text-align-lf"><?= StringUtils::amountFormat3($param['my_balance']) ?>元</div>
-        <div class="col-xs-3 safe-txt text-align-ct"><a href="/user/userbank/recharge?from=<?= urlencode('/order/order?sn='.$deal->sn)?>">去充值</a></div>
-    </div>
-    <form action="/order/order/doorder?sn=<?= $deal->sn ?>" method="post" id="orderform" data-to="1">
-        <input name="_csrf" type="hidden" id="_csrf" value="<?=Yii::$app->request->csrfToken ?>">
+
+    <?php if ($deal->allowUseCoupon) { ?>
         <div class="row sm-height border-bottom">
-            <div class="col-xs-4 safe-txt text-align-ct">投资金额</div>
-            <input name="money" type="number" id="money" value="<?= empty($money) ? '' : $money ?>" placeholder="请输入投资金额"  class="col-xs-6 safe-lf text-align-lf" step="any">
-            <div class="col-xs-2 safe-txt">元</div>
-        </div>
-
-        <?php if ($deal->allowUseCoupon) { ?>
-            <div class="row sm-height border-bottom">
-                <?php if (empty($couponId)) { ?>
-                    <div class="col-xs-4 safe-txt text-align-ct">使用代金券</div>
-                    <?php if (empty($coupon)) { ?>
-                    <div class="col-xs-8 safe-txt">无可用</div>
-                    <?php } else { ?>
-                    <div class="col-xs-8 safe-txt" onclick="toCoupon()"><span class="notice">请选择</span></div>
-                    <?php } ?>
+            <?php if (empty($couponId)) { ?>
+                <div class="col-xs-4 safe-txt text-align-ct">使用代金券</div>
+                <?php if (empty($coupon)) { ?>
+                <div class="col-xs-8 safe-txt">无可用</div>
                 <?php } else { ?>
-                    <div class="col-xs-4 safe-txt text-align-ct">代金券抵扣</div>
-                    <div class="col-xs-5 safe-txt" onclick="toCoupon()"><?= $coupon->couponType->amount ?>元</div>
-                    <div class="col-xs-3 safe-txt text-align-ct" onclick="resetCoupon()">清除</div>
-                    <input name="couponId" id="couponId" type="text" value="<?= $coupon->id ?>" hidden="hidden">
-                    <input name="couponMoney" id="couponMoney" type="text" value="<?= $coupon->couponType->amount ?>" hidden="hidden">
+                <div class="col-xs-8 safe-txt" onclick="toCoupon()"><span class="notice">请选择</span></div>
                 <?php } ?>
-            </div>
-        <?php } ?>
+            <?php } else { ?>
+                <div class="col-xs-4 safe-txt text-align-ct">代金券抵扣</div>
+                <div class="col-xs-5 safe-txt" onclick="toCoupon()"><?= $coupon->couponType->amount ?>元</div>
+                <div class="col-xs-3 safe-txt text-align-ct" onclick="resetCoupon()">清除</div>
+                <input name="couponId" id="couponId" type="text" value="<?= $coupon->id ?>" hidden="hidden">
+                <input name="couponMoney" id="couponMoney" type="text" value="<?= $coupon->couponType->amount ?>" hidden="hidden">
+            <?php } ?>
+        </div>
+    <?php } ?>
 
-        <div class="row shouyi">
-            <div class="col-xs-4 safe-lf text-align-ct">实际支付</div>
-            <div class="col-xs-8 safe-lf text-align-lf shijizhifu">0.00元</div>
-        </div>
-        <div class="row shouyi">
-            <div class="col-xs-4 safe-lf text-align-ct">预计收益</div>
-            <div class="col-xs-8 safe-lf text-align-lf yuqishouyi">0.00元</div>
-        </div>
-
-        <div class="row login-sign-btn ht">
-            <div class="col-xs-3"></div>
-            <div class="col-xs-6 text-align-ct">
-                <input id="buybtn" class="btn-common btn-normal" type="submit" style="background: #F2F2F2;" value="购买">
-            </div>
-            <div class="col-xs-3"></div>
-        </div>
-        <div class="row surplus">
-            <center><a class="col-xs-12" href="/order/order/agreement?id=<?= $deal->id ?>">查看“产品合同”</a></center>
-        </div>
-    </form>
-
-    <!-- 遮罩层 start  -->
-    <div class="mask"></div>
-    <!-- 遮罩层 end  -->
-    <!-- 输入弹出框 start  -->
-    <div class="succeed-info hidden">
-        <div class="col-xs-12"><img src="<?= ASSETS_BASE_URI ?>images/succeed.png" alt="对钩"> </div>
-        <div class="col-xs-12">购买成功</div>
+    <div class="row shouyi">
+        <div class="col-xs-4 safe-lf text-align-ct">实际支付</div>
+        <div class="col-xs-8 safe-lf text-align-lf shijizhifu">0.00元</div>
     </div>
-    <!-- 输入弹出框 end  -->
-    <!-- 购买页 end  -->
+    <div class="row shouyi">
+        <div class="col-xs-4 safe-lf text-align-ct">预计收益</div>
+        <div class="col-xs-8 safe-lf text-align-lf yuqishouyi">0.00元</div>
+    </div>
 
-    <script type="text/javascript">
-        function toCoupon()
-        {
-            var money = $('#money').val();
-            var url = '/user/coupon/valid?sn=<?= $deal->sn ?>&money='+money;
+    <div class="row login-sign-btn ht">
+        <div class="col-xs-3"></div>
+        <div class="col-xs-6 text-align-ct">
+            <input id="buybtn" class="btn-common btn-normal" type="submit" style="background: #F2F2F2;" value="购买">
+        </div>
+        <div class="col-xs-3"></div>
+    </div>
+    <div class="row surplus">
+        <center><a class="col-xs-12" href="/order/order/agreement?id=<?= $deal->id ?>">查看“产品合同”</a></center>
+    </div>
+</form>
 
-            location.href = url;
-        }
+<!-- 遮罩层 start  -->
+<div class="mask"></div>
+<!-- 遮罩层 end  -->
+<!-- 输入弹出框 start  -->
+<div class="succeed-info hidden">
+    <div class="col-xs-12"><img src="<?= ASSETS_BASE_URI ?>images/succeed.png" alt="对钩"> </div>
+    <div class="col-xs-12">购买成功</div>
+</div>
+<!-- 输入弹出框 end  -->
+<!-- 购买页 end  -->
 
-        function resetCoupon()
-        {
-            var money = $('#money').val();
-            var url = '/order/order?sn=<?= $deal->sn ?>&money='+money;
+<script type="text/javascript">
+    function toCoupon()
+    {
+        var money = $('#money').val();
+        var url = '/user/coupon/valid?sn=<?= $deal->sn ?>&money='+money;
 
-            location.replace(url);
-        }
-    </script>
+        location.href = url;
+    }
+
+    function resetCoupon()
+    {
+        var money = $('#money').val();
+        var url = '/order/order?sn=<?= $deal->sn ?>&money='+money;
+
+        location.replace(url);
+    }
+</script>
