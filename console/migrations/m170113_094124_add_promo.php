4805777c2ea636ca6418fecc464cac9ed3996bc9
shiyang <shiyang@wangcaigu.com>
2017-01-14 13:46:22 +0800

    更改Migration
    Summary: 更改Migration 中 startTime 字段
    
    Test Plan: 更改migration
    
    Reviewers: modi
    
    Differential Revision: http://dx.wangcaigu.cn/D1772

--- console/migrations/m170113_094124_add_promo.php
+++ console/migrations/m170113_094124_add_promo.php
@@ -8,7 +8,7 @@ class m170113_094124_add_promo extends Migration
     {
         $this->insert('promo', [
             'title' => '首次投资送积分',
-            'starTime' => '2017-01-18 00:00:00',
+            'startTime' => '2017-01-18 00:00:00',
             'endTime' => '2017-02-08 23:59:59',
             'key' => 'first_order_point',
             'promoClass' => \common\models\promo\FirstOrderPoints::class,
