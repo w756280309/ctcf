91b624409da6f455207eb1966fa30aa764986bf8
shiyang <shiyang@wangcaigu.com>
2017-02-22 15:11:04 +0800

    wap端首页调整
    Summary:
    1. 将js移动到body最后边
    2. index.css 中内容直接嵌入html中
    3. 删除首页wenjfbase.css的引入
    4. banner图先只显示一张图片,剩余图片等页面加载完成之后用js替换
    5. 删除百度统计相关代码
    6. 删除首页微信分享逻辑(分享后是默认效果)
    
    Test Plan: 保证首页页面逻辑不受影响
    
    Reviewers: modi
    
    Subscribers: zuoyuwei
    
    Differential Revision: http://dx.wangcaigu.cn/D1910

---
+++ wap/views/layouts/normal.php
@@ -0,0 +1,54 @@
+<?php
+
+use common\models\growth\PageMeta;
+use common\view\AnalyticsHelper;
+use yii\helpers\Html;
+
+AnalyticsHelper::registerTo($this);
+
+$meta = PageMeta::getMeta(Yii::$app->request);
+
+if (null !== $meta) {
+    $keywords = Html::encode($meta->keywords);
+    $ctitle = Html::encode($meta->title);
+    $description = Html::encode($meta->description);
+} else {
+    $keywords = Yii::$app->params['wap_page_keywords'].','.trim($this->extraKeywords, ', ');
+    $description = Yii::$app->params['wap_page_descritpion'];
+}
+
+$this->registerMetaTag([
+    'name' => 'keywords',
+    'content' => $keywords,
+]);
+
+$this->registerMetaTag([
+    'name' => 'description',
+    'content' => $description,
+]);
+
+$this->registerJs('$(function () {
+                hmsr();
+                addToken();
+            });')
+
+?>
+
+<?php $this->beginPage(); ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
+        <meta name="renderer" content="webkit">
+        <meta name="format-detection" content="telephone=no"/>
+        <title><?= isset($ctitle) ? $ctitle : Html::encode($this->title) ?></title>
+        <?php $this->head(); ?>
+    </head>
+    <body>
+        <?php $this->beginBody(); ?>
+            <?= $content ?>
+        <?php $this->endBody(); ?>
+    </body>
+</html>
+<?php $this->endPage(); ?>
