61cf92ff617805f6d960ee3e4a84b98e86883111
zuoyuwei <zuoyuwei@wangcaigu.com>
2017-04-06 20:02:19 +0800

    世界卫生日活动改版
    Summary: 世界卫生日活动改版
    
    Test Plan: 修改了页面图片及文字，请仔细核对
    
    Reviewers: modi
    
    Differential Revision: http://dx.wangcaigu.cn/D2129

--- wap/modules/promotion/views/p1704/world_health_day.php
+++ wap/modules/promotion/views/p1704/world_health_day.php
@@ -6,7 +6,7 @@ $this->share = $share;
 ?>
 <link rel="stylesheet" href="<?= FE_BASE_URI ?>wap/common/css/wenjfbase.css">
 <link rel="stylesheet" href="<?= FE_BASE_URI ?>wap/common/css/activeComHeader.css">
-<link rel="stylesheet" href="<?= FE_BASE_URI ?>wap/campaigns/worldHealth-Day/css/index.css?v=1.1">
+<link rel="stylesheet" href="<?= FE_BASE_URI ?>wap/campaigns/worldHealth-Day/css/index.css?v=1.2">
 <script src="<?= FE_BASE_URI ?>libs/lib.flexible3.js"></script>
 
 <div class="flex-content">
@@ -20,24 +20,24 @@ $this->share = $share;
     <?php } ?>
 
     <div class="banner">
-        <img src="<?= FE_BASE_URI ?>wap/campaigns/worldHealth-Day/images/banner-01.png" alt="图">
-        <img src="<?= FE_BASE_URI ?>wap/campaigns/worldHealth-Day/images/banner-02.png" alt="图">
-        <img src="<?= FE_BASE_URI ?>wap/campaigns/worldHealth-Day/images/banner-03.png" alt="图">
-        <img src="<?= FE_BASE_URI ?>wap/campaigns/worldHealth-Day/images/banner-04.png" alt="图">
-        <img src="<?= FE_BASE_URI ?>wap/campaigns/worldHealth-Day/images/banner-05.png" alt="图">
-        <img src="<?= FE_BASE_URI ?>wap/campaigns/worldHealth-Day/images/banner-06.png" alt="图">
-        <img src="<?= FE_BASE_URI ?>wap/campaigns/worldHealth-Day/images/banner-07.png" alt="图">
-        <img src="<?= FE_BASE_URI ?>wap/campaigns/worldHealth-Day/images/banner-08.png" alt="图">
-        <img src="<?= FE_BASE_URI ?>wap/campaigns/worldHealth-Day/images/banner-09.png" alt="图">
-        <img src="<?= FE_BASE_URI ?>wap/campaigns/worldHealth-Day/images/banner-10.png" alt="图">
-        <img src="<?= FE_BASE_URI ?>wap/campaigns/worldHealth-Day/images/banner-11.png" alt="图">
+        <img src="<?= FE_BASE_URI ?>wap/campaigns/worldHealth-Day/images/banner_01.png" alt="图">
+        <img src="<?= FE_BASE_URI ?>wap/campaigns/worldHealth-Day/images/banner_02.png" alt="图">
+        <img src="<?= FE_BASE_URI ?>wap/campaigns/worldHealth-Day/images/banner_03.png" alt="图">
+        <img src="<?= FE_BASE_URI ?>wap/campaigns/worldHealth-Day/images/banner_04.png" alt="图">
+        <img src="<?= FE_BASE_URI ?>wap/campaigns/worldHealth-Day/images/banner_05.png" alt="图">
+        <img src="<?= FE_BASE_URI ?>wap/campaigns/worldHealth-Day/images/banner_06.png" alt="图">
+        <img src="<?= FE_BASE_URI ?>wap/campaigns/worldHealth-Day/images/banner_07.png" alt="图">
+        <img src="<?= FE_BASE_URI ?>wap/campaigns/worldHealth-Day/images/banner_08.png" alt="图">
+        <img src="<?= FE_BASE_URI ?>wap/campaigns/worldHealth-Day/images/banner_09.png" alt="图">
+        <img src="<?= FE_BASE_URI ?>wap/campaigns/worldHealth-Day/images/banner_10.png" alt="图">
+        <img src="<?= FE_BASE_URI ?>wap/campaigns/worldHealth-Day/images/banner_11.png" alt="图">
     </div>
 
     <div class="rule">
         <ol>
             <li class="f14">此活动为限时活动，仅限2017年4月7日至4月9日三天，以认购成功时间为准；</li>
             <li class="f14">本次活动面向所有温都金服注册用户；</li>
-            <li class="f14">活动期间每投资两万元(不含转让产品)，送积分400分，可兑换积分商城“卫生日专区”对应礼品：蓝月亮洗衣液1kg*2瓶、妈妈壹选洗手液250ml*2瓶等；</li>
+            <li class="f14">活动期间每投资两万元(不含转让产品)，送积分400分，可兑换积分商城“卫生日专区”对应礼品：蓝月亮洗衣液1kg、妈妈壹选洗手液250ml等；</li>
             <li class="f14">本次活动每位用户最多可以获得2000积分，积分将于投资成功后的三个工作日内发放；</li>
             <li class="f14">实物礼品仅支持线下领取，领取地址：温州市鹿城区飞霞南路657号保丰大楼4层。</li>
         </ol>
