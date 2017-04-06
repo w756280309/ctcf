9813c34971c2b2bad7eab14ea95b33f9bc5f360f
zuoyuwei <zuoyuwei@wangcaigu.com>
2017-04-06 13:26:26 +0800

    增加世界卫生日活动页面
    Summary: 增加世界卫生日活动页面
    
    Test Plan:
    # 对照UI图查看具体页面；
    # 页面地址：/promotion/p1704/world-health-day
    
    Reviewers: modi
    
    Differential Revision: http://dx.wangcaigu.cn/D2122

---
+++ wap/modules/promotion/controllers/P1704Controller.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace wap\modules\promotion\controllers;
+
+use common\controllers\HelpersTrait;
+use common\models\adv\Share;
+use yii\web\Controller;
+
+class P1704Controller extends Controller
+{
+    use HelpersTrait;
+
+    public $layout = '@app/views/layouts/fe';
+
+    /**
+     * 世界卫生日活动.
+     */
+    public function actionWorldHealthDay($wx_share_key = null)
+    {
+        $share = null;
+
+        if (!empty($wx_share_key)) {
+            $share = Share::findOne(['shareKey' => $wx_share_key]);
+        }
+
+        return $this->render('world_health_day', [
+            'share' => $share,
+        ]);
+    }
+}
---
+++ wap/modules/promotion/views/p1704/world_health_day.php
@@ -0,0 +1,48 @@
+<?php
+
+$this->title = '关爱健康，轻松理财';
+$this->share = $share;
+
+?>
+<link rel="stylesheet" href="<?= FE_BASE_URI ?>wap/common/css/wenjfbase.css">
+<link rel="stylesheet" href="<?= FE_BASE_URI ?>wap/common/css/activeComHeader.css">
+<link rel="stylesheet" href="<?= FE_BASE_URI ?>wap/campaigns/worldHealth-Day/css/index.css?v=1.1">
+<script src="<?= FE_BASE_URI ?>libs/lib.flexible3.js"></script>
+
+<div class="flex-content">
+    <?php if (!defined('IN_APP')) { ?>
+        <div class="header">
+            <ul class="clearfix">
+                <li class="lf f16"><img src="<?= FE_BASE_URI ?>wap/wendumao/images/logo.png" alt="">温都金服国资平台</li>
+                <li class="rg f13"><a class="" href="/">返回首页</a></li>
+            </ul>
+        </div>
+    <?php } ?>
+
+    <div class="banner">
+        <img src="<?= FE_BASE_URI ?>wap/campaigns/worldHealth-Day/images/banner-01.png" alt="图">
+        <img src="<?= FE_BASE_URI ?>wap/campaigns/worldHealth-Day/images/banner-02.png" alt="图">
+        <img src="<?= FE_BASE_URI ?>wap/campaigns/worldHealth-Day/images/banner-03.png" alt="图">
+        <img src="<?= FE_BASE_URI ?>wap/campaigns/worldHealth-Day/images/banner-04.png" alt="图">
+        <img src="<?= FE_BASE_URI ?>wap/campaigns/worldHealth-Day/images/banner-05.png" alt="图">
+        <img src="<?= FE_BASE_URI ?>wap/campaigns/worldHealth-Day/images/banner-06.png" alt="图">
+        <img src="<?= FE_BASE_URI ?>wap/campaigns/worldHealth-Day/images/banner-07.png" alt="图">
+        <img src="<?= FE_BASE_URI ?>wap/campaigns/worldHealth-Day/images/banner-08.png" alt="图">
+        <img src="<?= FE_BASE_URI ?>wap/campaigns/worldHealth-Day/images/banner-09.png" alt="图">
+        <img src="<?= FE_BASE_URI ?>wap/campaigns/worldHealth-Day/images/banner-10.png" alt="图">
+        <img src="<?= FE_BASE_URI ?>wap/campaigns/worldHealth-Day/images/banner-11.png" alt="图">
+    </div>
+
+    <div class="rule">
+        <ol>
+            <li class="f14">此活动为限时活动，仅限2017年4月7日至4月9日三天，以认购成功时间为准；</li>
+            <li class="f14">本次活动面向所有温都金服注册用户；</li>
+            <li class="f14">活动期间每投资两万元(不含转让产品)，送积分400分，可兑换积分商城“卫生日专区”对应礼品：蓝月亮洗衣液1kg*2瓶、妈妈壹选洗手液250ml*2瓶等；</li>
+            <li class="f14">本次活动每位用户最多可以获得2000积分，积分将于投资成功后的三个工作日内发放；</li>
+            <li class="f14">实物礼品仅支持线下领取，领取地址：温州市鹿城区飞霞南路657号保丰大楼4层。</li>
+        </ol>
+        <a class="a-invest f18" href="/deal/deal/index">去理财</a>
+        <p class="f11 ownership">*本次活动最终解释权归温都金服所有</p>
+        <div class="bottom-img"></div>
+    </div>
+</div>
