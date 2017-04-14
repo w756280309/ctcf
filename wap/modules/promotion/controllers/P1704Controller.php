0b3653fc65ff31d95d53244a89cd577e622e50de
zuoyuwei <zuoyuwei@wangcaigu.com>
2017-04-14 17:01:35 +0800

    增加双倍积分活动
    Summary: # 增加双倍积分活动；
    
    Test Plan: 活动相对地址：/promotion/p1704/double-points
    
    Reviewers: modi
    
    Differential Revision: http://dx.wangcaigu.cn/D2193

--- wap/modules/promotion/controllers/P1704Controller.php
+++ wap/modules/promotion/controllers/P1704Controller.php
@@ -27,4 +27,20 @@ class P1704Controller extends Controller
             'share' => $share,
         ]);
     }
+
+    /**
+     * 双倍积分活动.
+     */
+    public function actionDoublePoints($wx_share_key = null)
+    {
+        $share = null;
+
+        if (!empty($wx_share_key)) {
+            $share = Share::findOne(['shareKey' => $wx_share_key]);
+        }
+
+        return $this->render('double_points', [
+            'share' => $share,
+        ]);
+    }
 }
