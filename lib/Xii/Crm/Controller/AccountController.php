aa7ce6e0acac6e0a39fc73fe623adcbf65de6cad
liufengjun <liufengjun@wangcaigu.com>
2017-04-24 20:59:20 +0800

    一部分列表

--- lib/Xii/Crm/Controller/AccountController.php
+++ lib/Xii/Crm/Controller/AccountController.php
@@ -3,12 +3,48 @@
 namespace Xii\Crm\Controller;
 
 
+use common\models\user\User;
+//use common\models\user\UserAccount;
+use common\models\user\UserInfo;
+use Xii\Crm\Model\Account;
+//use Xii\Crm\Model\Contact;
+use yii\data\ActiveDataProvider;
+//use yii\data\ArrayDataProvider;
 use yii\web\Controller;
 
 class AccountController extends Controller
 {
     public function actionIndex()
     {
-        return $this->render('index');
+        $u = User::tableName();
+        $ui = UserInfo::tableName();
+        $a = Account::tableName();
+        $query = Account::find()
+            ->leftJoin($u, "$u.crmAccount_id = $a.id")
+            ->leftJoin($ui, "$ui.user_id = $u.id")
+            ->orderBy(["$ui.lastInvestDate" => SORT_DESC]);
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+            'pagination' => [
+                'pageSize' => 15,
+            ]
+        ]);
+
+        $records = $dataProvider->getModels();
+        $data = [];
+        foreach ($records as $k=>$record) {
+            $identity = $record->getIdentity();
+            if (isset($identity->info)) {
+                $data[$k]['investCount'] = $record->info->investCount;
+                $data[$k]['investTotal'] = $record->info->investTotal;
+                $data[$k]['annualInvestment'] = $record->annualInvestment;
+            }
+            if (isset($identity->lendAccount)) {
+                $data[$k]['available_balance'] = $record->lendAccount->available_balance;
+                $data[$k]['investmentBalance'] = $record->lendAccount->investmentBalance;
+            }
+        }
+        return $this->render('index', ['dataProvider' => $dataProvider]);
     }
 }
--- lib/Xii/Crm/views/account/index.php
+++ lib/Xii/Crm/views/account/index.php
@@ -1,5 +1,69 @@
 <?php
+
 $this->title = '客户列表';
 $this->params['breadcrumbs'][] = ['label' => '客户列表', 'url' => '/cms/account'];
+
+use yii\grid\GridView;
 ?>
 
+<?= GridView::widget([
+    'dataProvider' => $dataProvider,
+    'layout' => '{items}',
+    'columns' => [
+        [
+            'attribute' => '姓名',
+            'value' => function ($model) {
+                return $model->user
+            }
+        ],
+        [
+            'attribute' => '手机',
+            'value' => function ($model) {
+            }
+        ],
+        [
+            'attribute' => '固定电话',
+            'value' => function ($model) {
+            }
+        ],
+        [
+            'attribute' => '年龄',
+            'value' => function ($model) {
+            }
+        ],
+        [
+            'attribute' => '性别',
+            'value' => function ($model) {
+            }
+        ],
+        [
+            'attribute' => '投资次数',
+            'value' => function ($model) {
+            }
+        ],
+        [
+            'attribute' => '累计投资金额',
+            'value' => function ($model) {
+
+            }
+        ],
+        [
+            'attribute' => '累计年化金额',
+            'value' => function ($model) {
+
+            }
+        ],
+        [
+            'attribute' => '理财资产',
+            'value' => function ($model) {
+
+            }
+        ],
+        [
+            'attribute' => '账户余额',
+            'value' => function ($model) {
+
+            }
+        ],
+    ]
+]) ?>
