3a87df81c4bad77d4e7d27e44766a9d1d542b7c4
liufengjun <liufengjun@wangcaigu.com>
2017-04-25 15:35:34 +0800

    潜客筛选及后台添加入口
    Summary: 潜客筛选及后台添加入口
    
    Test Plan: 潜客筛选及后台添加入口
    
    Reviewers: modi
    
    Subscribers: liufengjun
    
    Differential Revision: http://dx.wangcaigu.cn/D2261

--- lib/Xii/Crm/Controller/AccountController.php
+++ lib/Xii/Crm/Controller/AccountController.php
@@ -4,27 +4,65 @@ namespace Xii\Crm\Controller;
 
 use common\models\user\User;
 use common\models\user\UserInfo;
+use common\utils\SecurityUtils;
 use Xii\Crm\Model\Account;
 use Xii\Crm\Model\Contact;
 use Xii\Crm\Model\Identity;
+use Yii;
 use yii\data\ActiveDataProvider;
 use yii\web\Controller;
 
 class AccountController extends Controller
 {
-    public function actionIndex($isConverted = null)
+    public function actionIndex()
     {
+        $request = Yii::$app->request->get();
+        $mobile = !empty($request['mobile']) ? trim($request['mobile']) : '';
+        $landline = !empty($request['landline']) ? trim($request['landline']) : '';
+        $isConverted = isset($request['isConverted']) ? $request['isConverted'] : '';
+
         $u = User::tableName();
         $ui = UserInfo::tableName();
         $a = Account::tableName();
+        $c = Contact::tableName();
+        $leftJoinContact = false;
         $query = Account::find()
             ->leftJoin($u, "$u.crmAccount_id = $a.id")
             ->leftJoin($ui, "$ui.user_id = $u.id");
 
-        if (null !== $isConverted) {
-            $isConverted = $isConverted === 'false' ? false : (bool) $isConverted;
-            $query->where(["$a.isConverted" => $isConverted]);
+        if ('' !== $mobile) {
+            $query->leftJoin($c, "$c.account_id = $a.id");
+            $query->andWhere([
+                "$c.encryptedNumber" => SecurityUtils::encrypt($mobile),
+                "$c.type" => Contact::TYPE_MOBILE,
+            ]);
+            $leftJoinContact = true;
         }
+
+        if ('' !== $landline) {
+            if ('0' !== substr($landline, 0, 1)) {
+                $landline = '0577-' . $landline;
+            }
+
+            if ($leftJoinContact) {
+                $query->orWhere([
+                    "$c.encryptedNumber" => SecurityUtils::encrypt($landline),
+                    "$c.type" => Contact::TYPE_LANDLINE,
+                ]);
+            } else {
+                $query->leftJoin($c, "$c.account_id = $a.id");
+                $query->andWhere([
+                    "$c.encryptedNumber" => SecurityUtils::encrypt($landline),
+                    "$c.type" => Contact::TYPE_LANDLINE,
+                ]);
+            }
+        }
+
+        if ('' !== $isConverted) {
+            $isConverted = $isConverted === 'false' ? false : (bool) $request['isConverted'];
+            $query->andWhere(["$a.isConverted" => $isConverted]);
+        }
+
         $query->orderBy(["$ui.lastInvestDate" => SORT_DESC, "$a.id" => SORT_DESC]);
 
         $dataProvider = new ActiveDataProvider([
@@ -38,18 +76,14 @@ class AccountController extends Controller
         $data = [];
         foreach ($records as $record) {
             $identity = $record->getIdentity();
-            if (null === $identity) {
-                $data[$record->id] = [];
-                continue;
-            }
+            $contactMobile = Contact::findContactByAccountId($record->id)->andWhere(['type' => Contact::TYPE_MOBILE])->one();
+            $contactLandline = Contact::findContactByAccountId($record->id)->andWhere(['type' => Contact::TYPE_LANDLINE])->one();
+            $data[$record->id]['mobile'] = isset($contactMobile) ? $contactMobile->obfsNumber : null;
+            $data[$record->id]['landline'] = isset($contactLandline) ? $contactLandline->obfsNumber : null;
             if ($identity instanceof User || $identity instanceof Identity) {
                 $data[$record->id]['gender'] = $identity->getCrmGender();
                 $data[$record->id]['name'] = $identity->getCrmName();
                 $data[$record->id]['age'] = $identity->getCrmAge();
-                $contactMobile = Contact::findContactByAccountId($record->id)->andWhere(['type' => Contact::TYPE_MOBILE])->one();
-                $contactLandline = Contact::findContactByAccountId($record->id)->andWhere(['type' => Contact::TYPE_LANDLINE])->one();
-                $data[$record->id]['mobile'] = isset($contactMobile) ? $contactMobile->obfsNumber : null;
-                $data[$record->id]['landline'] = isset($contactLandline) ? $contactLandline->obfsNumber : null;
             }
             if ($identity instanceof User) {
                 $data[$record->id]['mobile'] = '*' . substr($identity->mobile, -4);
@@ -65,6 +99,11 @@ class AccountController extends Controller
             }
         }
 
-        return $this->render('index', ['dataProvider' => $dataProvider, 'data' => $data]);
+
+        return $this->render('index', [
+            'dataProvider' => $dataProvider,
+            'data' => $data,
+            'isConverted' => $isConverted,
+        ]);
     }
 }
--- lib/Xii/Crm/views/account/index.php
+++ lib/Xii/Crm/views/account/index.php
@@ -4,9 +4,39 @@ $this->title = '客户列表';
 $this->params['breadcrumbs'][] = ['label' => '客户列表', 'url' => '/crm/account'];
 
 use yii\grid\GridView;
+use yii\helpers\Html;
 ?>
 <div class="row">
     <a href="/crm/identity/create" class="btn btn-primary">录入潜客</a>
+    <form action="/crm/account/index" method="get" target="_self">
+        <table class="table">
+            <tbody>
+            <tr>
+                <td>
+                    <label for="">客户</label>
+                    <select name="isConverted" class="m-wrap span8">
+                        <option value="">未选择</option>
+                        <option value="true" <?= $isConverted ? 'selected="selected"' : '' ?>>注册用户</option>
+                        <option value="false" <?= false === $isConverted && '' !== $isConverted ? 'selected="selected"' : '' ?>>潜在客户</option>
+                    </select>
+                </td>
+                <td>
+                    <label for="">手机</label>
+                    <input type="text" name="mobile" value="<?= Html::encode(trim(Yii::$app->request->get('mobile'))) ?>">
+                </td>
+                <td>
+                    <label for="">固定电话</label>
+                    <input type="text" name="landline" value="<?= Html::encode(trim(Yii::$app->request->get('landline'))) ?>">
+                </td>
+                <td>
+                    <div class="search-btn" align="right">
+                        <button type='submit' class="btn blue btn-block button-search">搜索 <i class="m-icon-swapright m-icon-white"></i></button>
+                    </div>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    </form>
 </div>
 <div class="row">
     <?= GridView::widget([
