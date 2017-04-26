646201987dabc12b0daef3d6bea5cf7e09322907
Mo Di <modi@wangcaigu.com>
2017-04-26 16:14:58 +0800

    hotfix: 定义const以用于TablesawAsset成员变量初始化

--- lib/Zii/Asset/Tablesaw/TablesawAsset.php
+++ lib/Zii/Asset/Tablesaw/TablesawAsset.php
@@ -4,12 +4,14 @@ namespace Zii\Asset\Tablesaw;
 
 use yii\web\AssetBundle;
 
+define('ZII_TABLESAW_SOURCE_PATH', __DIR__.'/source');
+
 /**
  * Asset bundle for https://github.com/filamentgroup/tablesaw/tree/v3.0.0
  */
 class TablesawAsset extends AssetBundle
 {
-    public $sourcePath = __DIR__.'/source';
+    public $sourcePath = ZII_TABLESAW_SOURCE_PATH;
     public $css = [
         'tablesaw.css',
     ];
