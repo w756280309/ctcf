46541d12f57c2ddf82ba840dc95a5d5cc85c891d
zuoyuwei <zuoyuwei@wangcaigu.com>
2017-05-16 17:07:00 +0800

    修改夺宝页面，显示最终获奖结果
    Summary: # 修改夺宝页面，显示最终获奖结果
    
    Test Plan: 链接地址：/promotion/p1705/duobao
    
    Differential Revision: http://dx.wangcaigu.cn/D2362

--- wap/modules/promotion/views/p1705/duobao.php
+++ wap/modules/promotion/views/p1705/duobao.php
@@ -1,6 +1,5 @@
 <?php
 
-use common\utils\SecurityUtils;
 use common\utils\StringUtils;
 
 $this->title = '0元夺宝送iPhone7';
@@ -8,14 +7,11 @@ $this->share = $share;
 $this->headerNavOn = true;
 
 $isLogin = !\Yii::$app->user->isGuest;
-$next = Yii::$app->request->hostInfo.'/promotion/p1705/duobao';
 
 ?>
 <link rel="stylesheet" href="<?= FE_BASE_URI ?>wap/common/css/wenjfbase.css?v=1.0">
-<link rel="stylesheet" href="<?= FE_BASE_URI ?>wap/campaigns/active20170504/css/index.css?v=1.1">
+<link rel="stylesheet" href="<?= FE_BASE_URI ?>wap/campaigns/active20170504/css/index.css?v=20170516">
 <script src="<?= FE_BASE_URI ?>libs/lib.flexible3.js"></script>
-<script  src="<?= FE_BASE_URI ?>libs/fastclick.js"></script>
-<script src="<?= FE_BASE_URI ?>wap/common/js/com.js"></script>
 
 <div class="flex-content">
     <div class="banner">
@@ -23,6 +19,7 @@ $next = Yii::$app->request->hostInfo.'/promotion/p1705/duobao';
     </div>
     <div class="gift">
         <h5>奖品：iPhone7 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;数量：1部</h5>
+        <img class="guoqi" src="<?= FE_BASE_URI ?>wap/campaigns/active20170504/img/guoqi.png" alt="">
         <img src="<?= FE_BASE_URI ?>wap/campaigns/active20170504/img/phone.png" alt="">
         <div class="progress">
             <p class="progressTotal"><span class="progressLine" style="width: <?= $jindu ?>%;"></span></p>
@@ -32,7 +29,7 @@ $next = Yii::$app->request->hostInfo.'/promotion/p1705/duobao';
             <?php if ($isLogin && $isJoinWith && $joinTicket) { ?>
                 <button class="join joined">夺宝码：<?= $joinTicket->getCode() ?></button>
             <?php } else { ?>
-                <button class="join-btn">参与夺宝</button>
+                <button class="join-btn joined">参与夺宝</button>
             <?php } ?>
         </div>
     </div>
@@ -88,197 +85,4 @@ $next = Yii::$app->request->hostInfo.'/promotion/p1705/duobao';
     <p class="text-align-ct tips">本次活动最终解释权归温都金服所有<br>
         理财非存款 产品有风险 投资须谨慎
     </p>
-    <div class="mask"></div>
-    <div class="pomp note">
-        <img class="close" src="<?= FE_BASE_URI ?>wap/campaigns/active20170504/img/close.png" alt="">
-        <p>活动未开始</p>
-    </div>
-    <div class="pomp noChange">
-        <img class="close" src="<?= FE_BASE_URI ?>wap/campaigns/active20170504/img/close.png" alt="">
-        <img style="width: 3.653333rem;" src="<?= FE_BASE_URI ?>wap/campaigns/active20170504/img/no-change.png" alt="">
-        <p>您的手机号段没有夺宝资格去看看其他活动吧！</p>
-    </div>
-    <div class="pomp login">
-        <img class="close" src="<?= FE_BASE_URI ?>wap/campaigns/active20170504/img/close.png" alt="">
-        <img style="width: 1.3466667rem;" src="<?= FE_BASE_URI ?>wap/campaigns/active20170504/img/login.png" alt="">
-        <p>您还没有登录哦<br>快去登录获得夺宝机会吧！</p>
-        <a href="/site/login?next=<?= urlencode($next) ?>">去登录</a>
-    </div>
-    <div class="pomp address">
-        <img class="close" src="<?= FE_BASE_URI ?>wap/campaigns/active20170504/img/close.png" alt="">
-        <img style="width: 1.3466667rem;" src="<?= FE_BASE_URI ?>wap/campaigns/active20170504/img/address.png" alt="">
-        <p>完善个人信息<br>就有机会拿走大奖了哦！</p>
-        <a href="/user/bank">去完善</a>
-    </div>
-    <div class="pomp invite">
-        <img class="close" src="<?= FE_BASE_URI ?>wap/campaigns/active20170504/img/close.png" alt="">
-        <img style="width: 4.2533333rem;" src="<?= FE_BASE_URI ?>wap/campaigns/active20170504/img/invite.png" alt="">
-        <p>您还没有参与机会<br>快去邀请好友吧！</p>
-        <a href="/user/invite">去邀请</a>
-    </div>
-    <div class="bing-info pop">
-        <div class="bing-tishi">请输入领取的手机号码</div>
-        <p class="tishi-p" style="margin: 20px auto !important;">
-            <input class="f15" type="tel" maxlength="11" id="mobile">
-            <span style="color: red;"></span>
-        </p>
-        <div class="bind-btn"><span class="true">确定</span></div>
-    </div>
 </div>
-<script>
-    $(function () {
-        FastClick.attach(document.body);
-
-        var isGuest = '<?= !$isLogin ?>';
-        var promoTime = '<?= $promoTime ?>';
-        var isBind = '<?= $isBind ?>';
-        var isJoinWith = '<?= $isJoinWith ?>';
-
-        if (isGuest) {
-            if ('1' === promoTime) {
-                note('活动未开始');
-            } else if ('3' === promoTime) {
-                note('活动已结束');
-            } else {
-                mobilePop();
-            }
-        } else {
-            if (isJoinWith && !isBind) {
-                bind();
-            }
-        }
-
-        $('#mobile').on('keyup',function() {
-            var num = moduleFn.clearNonum($(this));
-        });
-
-        $(".close").on("click",function() {
-            $(".pomp,.mask,.pop").hide();
-            $('body').off('touchmove');
-        });
-
-        var allowClick = true;
-
-        $(".bind-btn").on("click",function() {
-            if (!allowClick) {
-                return;
-            }
-
-            if (validateMobile()) {
-                allowClick = false;
-
-                var key = '<?= $promo->key ?>';
-                var phonenum =  $('#mobile').val();
-                var xhr = $.get('/promotion/p1705/validate-mobile?key='+key+'&mobile='+phonenum);
-
-                xhr.done(function(data) {
-                    if (data.code) {
-                        if ('undefined' !== typeof data.message && '' !== data.message) {
-                            $('.tishi-p span').html(data.message);
-                        }
-                    } else {
-                        $(".pomp,.mask,.pop").hide();
-                        $('body').off('touchmove');
-                    }
-
-                    allowClick = true;
-                });
-
-                xhr.fail(function () {
-                    note('系统繁忙,请稍后重试!');
-                    allowClick = true;
-                })
-            }
-        });
-
-        $(".join-btn").on("click", function(e) {
-            e.preventDefault();
-
-            if (!allowClick) {
-                return;
-            }
-
-            var xhr = $.get('/promotion/p1705/duobao');
-            allowClick = false;
-
-            xhr.done(function(data) {
-                if (0 === data.code) {
-                    var toUrl = '<?= Yii::$app->request->absoluteUrl ?>';
-
-                    if ('undefined' !== typeof data.toUrl && '' !== data.toUrl) {
-                        toUrl = data.toUrl;
-                    }
-
-                    location.replace(toUrl);
-                } else if (1 === data.code) {
-                    note('活动未开始');
-                } else if (2 === data.code) {
-                    note('活动已结束');
-                } else if (3 === data.code) {
-                    mobileFail();
-                } else if (4 === data.code) {
-                    invite();
-                } else if (5 === data.code) {
-                    login();
-                }
-
-                allowClick = true;
-            });
-
-            xhr.fail(function () {
-                note('系统繁忙,请稍后重试!');
-                allowClick = true;
-            });
-        });
-    });
-
-    function mobileFail() {
-        $('.noChange,.mask').show();
-        $('body').on('touchmove', eventTarget, false);
-    }
-
-    function login() {
-        $('.login,.mask').show();
-        $('body').on('touchmove', eventTarget, false);
-    }
-
-    function invite() {
-        $('.invite,.mask').show();
-        $('body').on('touchmove', eventTarget, false);
-    }
-
-    function bind() {
-        $('.address,.mask').show();
-        $('body').on('touchmove', eventTarget, false);
-    }
-
-    function note(msg) {
-        $('.note p').html(msg);
-        $('.note, .mask').show();
-        $('body').on('touchmove', eventTarget, false);
-    }
-
-    function mobilePop() {
-        $('.bing-info,.mask').show();
-        $('body,html').scrollTop(0);
-        $('body').on('touchmove', eventTarget, false);
-    }
-
-    function validateMobile() {
-        var phonenum = $('#mobile').val();
-
-        if (phonenum.length < 11) {
-            $('.tishi-p span').html('请输入正确的手机号');
-
-            return false;
-        }
-
-        if (!moduleFn.check.mobile(phonenum)) {
-            $('.tishi-p span').html('手机号格式不正确');
-
-            return false;
-        }
-
-        return true;
-    }
-</script>
