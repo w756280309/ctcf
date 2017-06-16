0f0e159a9b1d20ee2e3821ae01739a29d4b2f823
zuoyuwei <zuoyuwei@wangcaigu.com>
2017-06-17 00:25:32 +0800

    修改微信推送消息模板颜色配置
    Summary: 修改微信推送消息模板颜色配置
    
    Test Plan: 修改微信推送消息模板颜色配置
    
    Differential Revision: http://dx.wangcaigu.cn/D2511

--- common/models/message/OrderMessage.php
+++ common/models/message/OrderMessage.php
@@ -34,13 +34,13 @@ class OrderMessage extends WechatMessage
 //            'remark' => ['感谢您的投资，点击查看详情，如有疑问请致电：400-101-5151进行咨询。', 'black'],
 //        ];
         $this->data = [
-            'first' => ['尊敬的客户，您于'.date('Y-m-d H:i:s', $order->order_time).'在温都金服成功投资。', 'black'],
-            'keyword1' => [$loan->title, 'black'],
-            'keyword2' => [StringUtils::amountFormat2(bcmul($order->yield_rate, 100, 2)).'%', 'black'],
-            'keyword3' => [$duration['value'].$duration['unit'], 'black'],
-            'keyword4' => [StringUtils::amountFormat3($order->order_money).'元', 'black'],
-            'keyword5' => [StringUtils::amountFormat3($interest).'元', 'black'],
-            'remark' => ['感谢您的投资，点击查看详情，如有疑问请致电：400-101-5151进行咨询。', 'black'],
+            'first' => ['尊敬的客户，您于'.date('Y-m-d H:i:s', $order->order_time).'在温都金服成功投资。', '#000000'],
+            'keyword1' => [$loan->title, '#000000'],
+            'keyword2' => [StringUtils::amountFormat2(bcmul($order->yield_rate, 100, 2)).'%', '#000000'],
+            'keyword3' => [$duration['value'].$duration['unit'], '#000000'],
+            'keyword4' => [StringUtils::amountFormat3($order->order_money).'元', '#000000'],
+            'keyword5' => [StringUtils::amountFormat3($interest).'元', '#000000'],
+            'remark' => ['感谢您的投资，点击查看详情，如有疑问请致电：400-101-5151进行咨询。', '#000000'],
         ];
         $this->user = $order->user;
         $this->linkUrl = Yii::$app->params['clientOption']['host']['wap'].'deal/deal/detail?sn='.$loan->sn;
--- common/models/message/PointMessage.php
+++ common/models/message/PointMessage.php
@@ -15,9 +15,9 @@ class PointMessage extends WechatMessage
     public function __construct(PointRecord $record)
     {
         $this->data = [
-            'first' => ['恭喜您成功绑定账户，获得'.$record->incr_points.'积分奖励！', 'black'],
-            'keyword1' => [$record->incr_points, 'black'],
-            'keyword2' => [PointRecord::getTypeName($record->ref_type), 'black'],
+            'first' => ['恭喜您成功绑定账户，获得'.$record->incr_points.'积分奖励！', '#000000'],
+            'keyword1' => [$record->incr_points, '#000000'],
+            'keyword2' => [PointRecord::getTypeName($record->ref_type), '#000000'],
         ];
         $this->user = $record->user;
         $this->templateId = Yii::$app->params['wx.msg_tpl.draw_success'];
