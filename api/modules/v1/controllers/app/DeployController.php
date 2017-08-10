773355ab2c4568564b455cc76f03e0b923536148
shiyang <shiyang@wangcaigu.com>
2017-08-10 15:24:08 +0800

    android 强制升级到 1.6.2
    Summary: android 的转让列表有问题，需要强制升级
    
    Test Plan: android 强制升级到 1.6.2
    
    Reviewers: modi
    
    Differential Revision: https://dx.wangcaigu.cn/D2768

--- api/modules/v1/controllers/app/DeployController.php
+++ api/modules/v1/controllers/app/DeployController.php
@@ -44,13 +44,13 @@ class DeployController extends Controller
                 ];
             }
         } else {
-            if ($versionCode < 9) {
+            if ($versionCode < 10) {
                 $content = [
-                    'old_updatetype' => 0,  //旧版本更新类型 0:无更新;1:有更新,无提示;2:有更新,有提示;3:有更新,强制更新
+                    'old_updatetype' => 3,  //旧版本更新类型 0:无更新;1:有更新,无提示;2:有更新,有提示;3:有更新,强制更新
                     'updatedesc' => '账户功能增加安全性，优化产品部分交互体验',  //新版本更新说明
-                    'versioncode' => '9',   //新版本版本号
-                    'versionname' => '1.6.1', //新版本版本名称
-                    'downloadurl' => 'https://dapp.wenjf.com/wjf_v1.6.1.apk',   //新版本下载地址
+                    'versioncode' => '10',   //新版本版本号
+                    'versionname' => '1.6.2', //新版本版本名称
+                    'downloadurl' => 'https://dapp.wenjf.com/wjf_v1.6.2.apk',   //新版本下载地址, android 不支持 http 向 https 的跳转，协议必须严格匹配
                 ];
             }
         }
