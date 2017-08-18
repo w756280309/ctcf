2e2c26f431fd3a1dbf8e26214a5dfbfc1849cfad
Mo Di <modi@wangcaigu.com>
2017-08-18 19:36:36 +0800

    active20170817
    Summary:
    active20170817 dist生成
    
    Merge branch 'prod' of ssh://dx.wangcaigu.cn/source/wdjf into active20170817
    
    Test Plan: active20170817 dist生成
    
    Reviewers: liufengjun
    
    Subscribers: modi
    
    Differential Revision: https://dx.wangcaigu.cn/D2804

---
+++ wap/modules/promotion/controllers/LandingController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace wap\modules\promotion\controllers;
+
+use yii\web\Controller;
+
+class LandingController extends Controller
+{
+    public function actionHomecoming17()
+    {
+        return $this->render('homecoming17');
+    }
+}
