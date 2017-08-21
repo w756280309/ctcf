f36fc6081856a6525a9788447fcf5ba7efa8908d
liufengjun <liufengjun@wangcaigu.com>
2017-08-21 13:46:25 +0800

    积分商城每日上新需求 - 点击商品任意每个区域都可跳转
    Summary:
    积分活动上新点击整块跳转
    配置项添加页面背景图及banner图
    
    Test Plan:
    1）检验是否可以整块点击图片跳转
    2）确定对应跳转的图片链接正确
    
    Reviewers: modi
    
    Subscribers: liufengjun
    
    Differential Revision: https://dx.wangcaigu.cn/D2811

--- view/themes/wdjf/m/modules/promotion/normal/points_guide.php
+++ view/themes/wdjf/m/modules/promotion/normal/points_guide.php
@@ -2,45 +2,47 @@
 $this->title = '限时积分新品';
 ?>
 <link rel="stylesheet" href="<?= FE_BASE_URI ?>wap/common/css/wenjfbase.css">
-<link rel="stylesheet" href="<?= FE_BASE_URI ?>wap/point-shop/css/index.css">
+<link rel="stylesheet" href="<?= FE_BASE_URI ?>wap/point-shop/css/index.css?v=1">
 <script src="<?= FE_BASE_URI ?>libs/lib.flexible3.js"></script>
-<div class="flex-content">
-    <div class="part-one"></div>
-    <?php
-        $topBox = isset($detail['top']) ? $detail['top'] : '';
-        $middleBox = isset($detail['middle']) ? $detail['middle'] : '';
-        $bottomBox = isset($detail['bottom']) ? $detail['bottom'] : '';
-        $baseUrl = '/mall/portal/guest?dbredirect=';
-    ?>
+<?php
+    $bgColor = isset($detail['bgColor']) ? $detail['bgColor'] : '';
+    $topBox = isset($detail['top']) ? $detail['top'] : '';
+    $middleBox = isset($detail['middle']) ? $detail['middle'] : '';
+    $bottomBox = isset($detail['bottom']) ? $detail['bottom'] : '';
+    $banner = isset($detail['banner']) ? $detail['banner'] : '';
+    $baseUrl = '/mall/portal/guest?dbredirect=';
+?>
+<div class="flex-content" style="background:url(<?= FE_BASE_URI.$bgColor['url'] ?>);background-size:contain">
+    <div class="part-one" style="background: url(<?= FE_BASE_URI.$banner['url'] ?>) no-repeat;background-size: contain;"></div>
     <div class="part-two">
         <?php if (!empty($topBox)) { ?>
-        <div class="gift-outbox gift-one" style="padding-right: 0.1rem;">
-            <img src="<?= FE_BASE_URI.$topBox['imgUrl'] ?>" alt="" class="lf">
-            <p class="gift-name"><?= $topBox['mainTitle'] ?></p>
-            <p class="gift-introduce"><?= $topBox['subTitle'] ?></p>
-            <p class="gift-point"><?= $topBox['points'] ?></p>
-            <a href="<?= $baseUrl.$topBox['duibaUrl'] ?>" class="gift-button">立即抢购</a>
-        </div>
+            <a href="<?= $baseUrl.$topBox['duibaUrl'] ?>" class="gift-outbox gift-one" style="padding-right: 0.1rem;">
+                <img src="<?= FE_BASE_URI.$topBox['imgUrl'] ?>" alt="" class="lf">
+                <p class="gift-name"><?= $topBox['mainTitle'] ?></p>
+                <p class="gift-introduce"><?= $topBox['subTitle'] ?></p>
+                <p class="gift-point"><?= $topBox['points'] ?></p>
+                <div class="gift-button">立即抢购</div>
+            </a>
         <?php } ?>
         <div class="fix-gift clearfix">
             <?php foreach ($middleBox as $key => $box) { ?>
-            <div class="gift-box <?= $box['class'] ?>">
-                <img src="<?= FE_BASE_URI.$box['imgUrl'] ?>" alt="">
-                <p class="gift-name"><?= $box['mainTitle'] ?></p>
-                <p class="gift-introduce"><?= $box['subTitle'] ?></p>
-                <p class="gift-point"><?= $box['points'] ?></p>
-                <a href="<?= $baseUrl.$box['duibaUrl'] ?>" class="gift-button">立即抢购</a>
-            </div>
+                <a href="<?= $baseUrl.$box['duibaUrl'] ?>" class="gift-box fix-gift clearfix <?= $box['class'] ?>">
+                    <img src="<?= FE_BASE_URI.$box['imgUrl'] ?>" alt="">
+                    <p class="gift-name"><?= $box['mainTitle'] ?></p>
+                    <p class="gift-introduce"><?= $box['subTitle'] ?></p>
+                    <p class="gift-point"><?= $box['points'] ?></p>
+                    <div class="gift-button">立即抢购</div>
+                </a>
             <?php } ?>
         </div>
         <?php if (!empty($bottomBox)) { ?>
-        <div class="gift-outbox gift-six">
+        <a href="<?= $baseUrl.$bottomBox['duibaUrl'] ?>" class="gift-outbox gift-six">
             <img src="<?= FE_BASE_URI.$bottomBox['imgUrl'] ?>" alt="" class="rg">
             <p class="gift-name"><?= $bottomBox['mainTitle'] ?></p>
             <p class="gift-introduce"><?= $bottomBox['subTitle'] ?></p>
             <p class="gift-point"><?= $bottomBox['points'] ?></p>
-            <a href="<?= $baseUrl.$bottomBox['duibaUrl'] ?>" class="gift-button">立即抢购</a>
-        </div>
+            <div class="gift-button">立即抢购</div>
+        </a>
         <?php } ?>
     </div>
     <div class="part-three">
