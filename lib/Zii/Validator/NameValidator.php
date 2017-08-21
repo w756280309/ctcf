2d8327121afe33af1ff183ebfe66900721d2acc4
shiyang <shiyang@wangcaigu.com>
2017-08-21 09:47:30 +0800

    修改文案

--- lib/Zii/Validator/NameValidator.php
+++ lib/Zii/Validator/NameValidator.php
@@ -9,7 +9,7 @@ class NameValidator extends Validator
     public function validateAttribute($model, $attribute)
     {
         if (!preg_match("/^[\x{4e00}-\x{9fa5}]{1,6}$/u", $model->$attribute)) {
-            $this->addError($model, $attribute, '{attribute}真实姓名不合法');
+            $this->addError($model, $attribute, '真实姓名不合法');
         }
     }
 }
