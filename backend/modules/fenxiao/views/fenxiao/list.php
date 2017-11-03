55fdc451e3ff6f8b193bc507b56259f2e7e73f05
liwenjie <liwenjie@lianhejinxin.com>
2017-11-03 13:56:44 +0800

    调整分销商页面展现形式,添加搜索未命中提示信息
    Summary: 修改分销商页面展现形式，添加未命中提示信息
    
    Test Plan: 搜索不存在的分销商时，会提示暂无数据
    
    Reviewers: modi
    
    Differential Revision: https://dx.wangcaigu.cn/D3124

--- backend/modules/fenxiao/views/fenxiao/list.php
+++ backend/modules/fenxiao/views/fenxiao/list.php
@@ -1,18 +1,21 @@
 <?php
+
 use yii\widgets\LinkPager;
+
 ?>
 <?php $this->beginBlock('blockmain'); ?>
 
-<div class="container-fluid">
-    <div class="row-fluid">
-        <div class="span12">
-            <h3 class="page-title">
-                分销商管理 <small>运营模块</small>
-                <a href="/fenxiao/fenxiao/add" id="sample_editable_1_new" class="btn green" style="float: right;">
-                    <i class="icon-plus"></i> 添加分销商用户
-                </a>
-            </h3>
-            <ul class="breadcrumb">
+    <div class="container-fluid">
+        <div class="row-fluid">
+            <div class="span12">
+                <h3 class="page-title">
+                    分销商管理
+                    <small>运营模块</small>
+                    <a href="/fenxiao/fenxiao/add" id="sample_editable_1_new" class="btn green" style="float: right;">
+                        <i class="icon-plus"></i> 添加分销商用户
+                    </a>
+                </h3>
+                <ul class="breadcrumb">
                     <li>
                         <i class="icon-home"></i>
                         <a href="/adv/adv/index">运营管理</a>
@@ -25,71 +28,76 @@ use yii\widgets\LinkPager;
                     <li>
                         <a href="javascript:void(0);">分销商列表</a>
                     </li>
-            </ul>
-        </div>
+                </ul>
+            </div>
 
-        <!--search start-->
-        <div class="portlet-body">
-            <form action="/fenxiao/fenxiao/list" method="get" target="_self">
-                <table class="table">
-                    <tbody>
-                    <tr>
-                        <td><span class="title">分销商名称</span></td>
-                        <td>
-                            <input type="text" class="m-wrap span8" name='name' value="<?=Yii::$app->request->get('name')?>"/>
-                        </td>
-                        <td>
-                            <div class="search-btn" align="right">
-                                <button type='submit' class="btn blue btn-block button-search">搜索 <i class="m-icon-swapright m-icon-white"></i></button>
-                            </div>
-                        </td>
-                    </tr>
-                    </tbody>
-                </table>
-            </form>
-        </div>
-        <!--search end -->
+            <!--search start-->
+            <div class="portlet-body">
+                <form action="/fenxiao/fenxiao/list" method="get" target="_self">
+                    <table class="table">
+                        <tbody>
+                        <tr>
+                            <td width ='80px'><span class="title">分销商名称</span></td>
+                            <td>
+                                <input type="text" class="m-wrap span4 " style="margin-bottom: 0px;width:200px" name='name'
+                                       value="<?= Yii::$app->request->get('name') ?>" placeholder="请输入分销商名称" size="50px"/>
+                            </td>
+                            <td>
+                                <div class="search-btn" align="right">
+                                    <button type='submit' class="btn blue btn-block button-search">搜索 <i
+                                                class="m-icon-swapright m-icon-white"></i></button>
+                                </div>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </form>
+            </div>
+            <!--search end -->
 
-        <div class="portlet-body">
-            <table class="table table-striped table-bordered table-advance table-hover">
-                <thead>
+            <div class="portlet-body">
+                <table class="table table-striped table-bordered table-advance table-hover">
+                    <thead>
                     <tr>
                         <th>分销商名称</th>
                         <th>推荐媒体</th>
-                        <th><center>操作</center></th>
+                        <th>
+                            <center>操作</center>
+                        </th>
                     </tr>
-                </thead>
-                <tbody>
-                <?php foreach ($model as $key => $val) : ?>
-                    <tr>
-                        <td><?= $val['name'] ?></td>
-                        <td>
-                            <?php if (!empty($val->affiliator) && $val->affiliator->isRecommend) { ?>
-                                <i class="icon-ok green" style="color: green;"></i>
-                            <?php }?>
-                        <td>
-                            <center>
-                                <a href="/fenxiao/fenxiao/edit?id=<?= $val['id'] ?>" class="btn mini green">
-                                    <i class="icon-edit"></i> 编辑</a>
-                                <a href="#" class="btn mini green code" id="<?= $val['affiliator_id'] ?>">
-                                    <i class="icon-edit"></i>二维码（公众号）</a>
-                            </center>
-                        </td>
-                    </tr>
-                <?php endforeach; ?>
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                    <?php if (count($model) < 1) echo '<tr><td><font color="red">暂无数据</font></td></tr>'; ?>
+                    <?php foreach ($model as $key => $val) : ?>
+                        <tr>
+                            <td><?= $val['name'] ?></td>
+                            <td>
+                                <?php if (!empty($val->affiliator) && $val->affiliator->isRecommend) { ?>
+                                    <i class="icon-ok green" style="color: green;"></i>
+                                <?php } ?>
+                            <td>
+                                <center>
+                                    <a href="/fenxiao/fenxiao/edit?id=<?= $val['id'] ?>" class="btn mini green">
+                                        <i class="icon-edit"></i> 编辑</a>
+                                    <a href="#" class="btn mini green code" id="<?= $val['affiliator_id'] ?>">
+                                        <i class="icon-edit"></i>二维码（公众号）</a>
+                                </center>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+            <!--分页-->
+            <div class="pagination" style="text-align:center"><?= LinkPager::widget(['pagination' => $pages]); ?></div>
         </div>
-        <!--分页-->
-        <div class="pagination" style="text-align:center"><?= LinkPager::widget(['pagination' => $pages]); ?></div>
     </div>
-</div>
     <script>
-        $('.code').on('click', function(){
+        $('.code').on('click', function () {
             var id = $(this).attr('id');
-            $.get('/fenxiao/fenxiao/code', {'id':id}, function(str){
+            $.get('/fenxiao/fenxiao/code', {'id': id}, function (str) {
                 if (str.code == 1) {
-                    window.open('/fenxiao/fenxiao/code-view?ticket='+str.ticket);
+                    window.open('/fenxiao/fenxiao/code-view?ticket=' + str.ticket);
                 }
             })
         })
