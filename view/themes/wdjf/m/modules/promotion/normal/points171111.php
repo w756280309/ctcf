1d883db20c6c1df217f9bdf1a8b1ebb7286dc05a
liufengjun <liufengjun@wangcaigu.com>
2017-11-11 16:59:29 +0800

    秋收积分商城上新活动
    Summary: 秋收积分商城上新活动
    
    Test Plan: 秋收积分商城上新活动
    
    Reviewers: modi
    
    Subscribers: liufengjun
    
    Differential Revision: https://dx.wangcaigu.cn/D3171

---
+++ view/themes/wdjf/m/modules/promotion/normal/points171111.php
@@ -0,0 +1,19 @@
+<?php
+$this->title = '秋收季上新';
+?>
+<link rel="stylesheet" href="<?= FE_BASE_URI ?>wap/common/css/wenjfbase.css">
+<link rel="stylesheet" href="<?= FE_BASE_URI ?>wap/campaigns/shop20171111/css/index.css?v=1">
+<script src="<?= FE_BASE_URI ?>libs/lib.flexible3.js"></script>
+<div class="flex-content">
+    <div class="top-part"></div>
+    <div class="middle-part">
+        <a href="/site/app-download?redirect=/mall/portal/guest?dbredirect=https%3A%2F%2Fgoods.m.duiba.com.cn%2Fmobile%2FappItemDetail%3FappItemId%3D1480627" class="gift gift-one"></a>
+        <a href="/site/app-download?redirect=/mall/portal/guest?dbredirect=https%3A%2F%2Fgoods.m.duiba.com.cn%2Fmobile%2FappItemDetail%3FappItemId%3D1076295" class="gift gift-two"></a>
+        <a href="/site/app-download?redirect=/mall/portal/guest?dbredirect=https%3A%2F%2Fgoods.m.duiba.com.cn%2Fmobile%2FappItemDetail%3FappItemId%3D1268343" class="gift gift-three"></a>
+        <a href="/site/app-download?redirect=/mall/portal/guest?dbredirect=https%3A%2F%2Fgoods.m.duiba.com.cn%2Fmobile%2FappItemDetail%3FappItemId%3D855863" class="gift gift-four"></a>
+        <a href="/site/app-download?redirect=/mall/portal/guest?dbredirect=https%3A%2F%2Fgoods.m.duiba.com.cn%2Fmobile%2FappItemDetail%3FappItemId%3D1076283" class="gift gift-five"></a>
+        <a href="/site/app-download?redirect=/mall/portal/guest?dbredirect=https%3A%2F%2Fgoods.m.duiba.com.cn%2Fmobile%2FappItemDetail%3FappItemId%3D855866" class="gift gift-six"></a>
+    </div>
+    <div class="bottom-part"></div>
+    <div class="fixed-part"><a href="/deal/deal/index?next=/promotion/normal/mall1111"></a></div>
+</div>
