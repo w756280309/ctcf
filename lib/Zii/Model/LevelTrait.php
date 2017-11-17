8f5543c2477d8add6b6db3f463ecd4378e61e457
liufengjun <liufengjun@wangcaigu.com>
2017-11-17 16:41:25 +0800

    后台-投资用户信息导出及对应定时脚本改为只读库
    Summary:
    1）后台-投资用户信息导出
    2）每小时1次的全量投资数据导出excel文件
    
    Test Plan:
    1）后台-投资用户信息导出
    2）每小时1次的全量投资数据导出excel文件
    
    Reviewers: modi
    
    Subscribers: liufengjun
    
    Differential Revision: https://dx.wangcaigu.cn/D3210

--- lib/Zii/Model/LevelTrait.php
+++ lib/Zii/Model/LevelTrait.php
@@ -2,6 +2,8 @@
 
 namespace Zii\Model;
 
+use common\service\UserService;
+
 trait LevelTrait
 {
     /**
@@ -11,25 +13,6 @@ trait LevelTrait
      */
     public function getLevel()
     {
-        $level = 0;
-        $coins = $this->coins;
-
-        if ($coins >= 20 && $coins < 50) {
-            $level = 1;
-        } elseif ($coins >= 50 && $coins < 100) {
-            $level = 2;
-        } elseif ($coins >= 100 && $coins < 200) {
-            $level = 3;
-        } elseif ($coins >= 200 && $coins < 500) {
-            $level = 4;
-        } elseif ($coins >= 500 && $coins < 800) {
-            $level = 5;
-        } elseif ($coins >= 800 && $coins < 1500) {
-            $level = 6;
-        } elseif ($coins >= 1500) {
-            $level = 7;
-        }
-
-        return $level;
+        return UserService::calcUserLevel($this->coins);
     }
 }
