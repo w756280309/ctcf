8ff72e09e2be762669763c0aab0a346ec6549aef
Mo Di <modi@wangcaigu.com>
2017-12-15 13:19:54 +0800

    hotfix: 充值问题

--- common/service/AccountService.php
+++ common/service/AccountService.php
@@ -41,8 +41,9 @@ class AccountService
         //修改充值状态
         $db = Yii::$app->db;
         $transaction = $db->beginTransaction();
-        $sql = "update recharge_record set status = :status where status = 0";
+        $sql = "update recharge_record set status = :status where id = :id and status = 0";
         $affectedRows = $db->createCommand($sql, [
+            'id' => $recharge->id,
             'status' => RechargeRecord::STATUS_YES,
         ])->execute();
         if (0 === $affectedRows) {
