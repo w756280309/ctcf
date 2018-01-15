bc7a808dbbb6337b99d574dc26889fac7b105740
zoujianshuang <zoujianshuang@dingtalk.com>
2018-01-16 00:11:57 +0800

    统计账户合并后的累计年华排行
    Summary: 账户关联后，统计总的账户的累计年化排行
    
    Test Plan: 先取出线上累计年化，再取出线下的累计年化，然后合并，排序
    
    Reviewers: modi
    
    Subscribers: zoujianshuang
    
    Differential Revision: https://dx.wangcaigu.cn/D3457

--- console/controllers/RankListController.php
+++ console/controllers/RankListController.php
@@ -32,15 +32,12 @@ class RankListController extends Controller
         if ($now <= bcadd($end, 10 * 60 + 24 * 3600)) {
             $redis = Yii::$app->redis;
             //最后一个订单id，无变化则不重新计算
-            $lastOrderId = \Yii::$app->db->createCommand(
-                "select max(id) from online_order"
-            )->queryScalar();
-            if ($lastOrderId != $redis->get('last-order-id')) {
-                $db = \Yii::$app->db;
-                $sql = "
+            $db = \Yii::$app->db;
+            $sql = "
 select 
     sum(truncate((if(p.refund_method > 1, o.order_money*p.expires/12, o.order_money*p.expires/365)), 2)) as annual, 
-    u.safeMobile, 
+    u.safeMobile,
+    u.offlineUserId, 
     max(o.id) as oid 
 from online_order o 
 inner join online_product p 
@@ -51,21 +48,86 @@ where date(from_unixtime(o.order_time)) >= :startDate
     and date(from_unixtime(o.order_time)) <= :endDate 
     and o.status = 1
 group by u.id 
-order by annual desc, 
-    oid asc 
-limit :num";
-                $res = $db->createCommand($sql, [
-                    'startDate' => $this->startDate,
-                    'endDate' => $this->endDate,
-                    'num' => $this->num,
-                ])->queryAll();
-                $arr = [];
-                foreach ($res as $k => $v) {
-                    $arr[] = ['mobile' => SecurityUtils::decrypt($v['safeMobile']), 'annual' => $v['annual']];
+";
+            $res = $db->createCommand($sql, [
+                'startDate' => $this->startDate,
+                'endDate' => $this->endDate,
+            ])->queryAll();
+
+            $sqlOffline = "
+select 
+    sum(truncate((if(p.repaymentMethod > 1, o.money*p.expires*10000/12, o.money*p.expires*10000/365)), 2)) as annual, 
+    u.id,
+    u.mobile,
+    max(o.id) as offid
+from offline_order o 
+inner join offline_loan p 
+    on o.loan_id = p.id 
+inner join offline_user u 
+    on u.id = o.user_id 
+where date(o.orderDate) >= :startDate
+    and date(o.orderDate) <= :endDate 
+    and o.isDeleted = 0
+group by u.id
+";
+            $resOffline = $db->createCommand($sqlOffline, [
+                'startDate' => $this->startDate,
+                'endDate' => $this->endDate,
+            ])->queryAll();
+            //整理数据
+            foreach ($res as $k => $v) {
+                foreach ($resOffline as $m => $n) {
+                    if($v['offlineUserId'] == $n['id']) {
+                        $res[$k]['annual'] += $n['annual'];
+                        unset($resOffline[$m]);
+                    }
                 }
-                $redis->set('ranking-list', json_encode($arr));
-                $redis->set('last-order-id', $lastOrderId);
             }
+            $res = array_merge($res, $resOffline);
+            uasort($res, function($m ,$n) {
+                if ($m['annual'] > $n['annual']) {
+                    return 1;
+                } else if ($m['annual'] == $n['annual']) {
+                    if (isset($m['oid'])) {
+                        if (isset($n['oid'])) {
+                            if($m['oid'] < $n['oid']) {
+                                return 1;
+                            } else {
+                                return -1;
+                            }
+                        } else {
+                            return 1;
+                        }
+                    } else {
+                        if (isset($n['oid'])) {
+                            return -1;
+                        } else {
+                            if ($m['offid'] > $n['offid']) {
+                                return -1;
+                            } else {
+                                return 1;
+                            }
+                        }
+                    }
+
+                } else {
+                    return -1;
+                }
+            });
+            //获取规定的条数
+            $data = [];
+            $num = $this->num;
+            if (count($res) < $num) {
+                $num = count($res);
+            }
+            for ($i = 0; $i < $num; $i ++) {
+                $data[] = array_pop($res);
+            }
+            $arr = [];
+            foreach ($data as $k => $v) {
+                $arr[] = ['mobile' => isset($v['safeMobile']) ? SecurityUtils::decrypt($v['safeMobile']) : $v['mobile'], 'annual' => $v['annual']];
+            }
+            $redis->set('ranking-list', json_encode($arr));
         }
     }
 
@@ -74,5 +136,11 @@ limit :num";
         $redis = Yii::$app->redis;
         $redis->del('ranking-list');
         $redis->del('last-order-id');
+    }
+
+    public function actionGet()
+    {
+        $redis = Yii::$app->redis;
+        var_dump($redis->get('ranking-list'));
     }
 }
