c5ad9f208821c577bc34d50debf93a797765f3b5
songpeng <songpeng@njfae.cn>
2018-02-08 10:53:27 +0800

    首页弹窗压缩图片

--- view/themes/ctcf/fe/src/wap/index/mask/js/mask.js
+++ view/themes/ctcf/fe/src/wap/index/mask/js/mask.js
@@ -1,26 +1,47 @@
 $(function(){
-    // if($('.mask-box').css('display')=="block"){
-    //     // $('body').on()
-    //     console.log(11);
-    // }
     // 全局禁止滑动
-    function addMove(){
+    function removeMove(){
         $('.flex-content').on('touchmove',function(e){
             var e=e||window.event;
             e.preventDefault();
         });
     }
-    function removeMove(){
+    function clearMove(){
         $('.flex-content').off('touchmove');
         $('.mask-no-invest').css('display','none');
+        $('.mask-prize-hint').css('display','none');
+        $('.mask-login-invest').css('display','none');
     }
-   addMove();
+   removeMove();
     $('i.close-box').on('click',function(){
-        removeMove();
+        console.log(111);
+        clearMove();
     })
+    // 已登陆注册弹框
+    var mySwiper= new Swiper('.urgrade-swiper-contain', {
+        spaceBetween:100,
+        initialSlide :0,
+        speed: 500,
+        loop:false,
+        pagination : '.swiper-pagination',
+        autoplayDisableOnInteraction: false,
+        onSlideChangeEnd:function(){
+            if(mySwiper.activeIndex==0){
+                $('.mask-login-invest').find('.close-box').css('display','none');
+            }else if(mySwiper.activeIndex==1){
+                $('.mask-login-invest').find('.close-box').css('display','block');
+            }
+        }
 
+    });
+    // 点击查看升级红包显示下一个页面
+    $('.check-login-invest').on('click',function(){
+        mySwiper.slideTo(1, 500);
 
-
-
-
+    });
+    // 点击补偿规则返回上一个页面
+    $('u.get-prize-rules').on('click',function(){
+        mySwiper.slideTo(0, 500);
+        // $('.mask-login-invest').find('.close-box').css('display','block');
+    });
 })
