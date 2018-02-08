4d4a947109cea8e1ee93831369684a9ce1217c10
liangwei02 <liangwei02@wangcaigu.com>
2018-02-08 13:06:43 +0800

    楚天财富网关于我们修改完成

--- view/themes/ctcf_new/pc/views/helpcenter/contact.php
+++ view/themes/ctcf_new/pc/views/helpcenter/contact.php
@@ -17,16 +17,16 @@ $this->registerCssFile(ASSETS_BASE_URI.'css/help/contact.css', ['depends' => 'fr
                 </div>
                 <div class="contact-content">
                     <div class="location">
-                        <img  src="../../images/help/location.png">
+                        <img style="width: 664px;"  src="<?= ASSETS_BASE_URI ?>ctcf/images/help/location.png">
                     </div>
-                    <p>公司地址 : 温州市鹿城区飞霞南路657号保丰大楼四层</p>
-                    <p>工作时间 : 8:30-17:30（周一至周五）</p>
+                    <p>公司地址 : 武汉市武昌区东湖路181号楚天文化创意产业园区8号楼1层</p>
+                    <p>工作时间 : 9:00-20:00（周一至周日，假日例外） </p>
                     <p>客服电话 : <?= \Yii::$app->params['platform_info.contact_tel'] ?></p>
-                    <p>客服时间 : 8:30-20:00（周一至周日，假日例外）</p>
-                    <p>客服QQ  : 1430843929</p>
-                    <p>&nbsp;</p>
-                    <p>保丰门店 : 温州市鹿城区飞霞南路657号保丰大楼一层</p>
-                    <p>工作时间 : 8:30-17:30（周一至周日）</p>
+                    <p>客服时间 : 9:00-20:00（周一至周日，假日例外）</p>
+                    <p>客服QQ  : 474574526</p>
+<!--                    <p>&nbsp;</p>-->
+<!--                    <p>保丰门店 : 温州市鹿城区飞霞南路657号保丰大楼一层</p>-->
+<!--                    <p>工作时间 : 8:30-17:30（周一至周日）</p>-->
                 </div>
             </div>
         </div>
