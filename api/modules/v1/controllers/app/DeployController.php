b3331621b84f3240e35ac47df3855bbf58a7a1a8
liufengjun <1029468030@qq.com>
2018-03-15 16:31:40 +0800

    温都金服app版本2.1更新
    Summary: 子任务：app版本2.1更新接口提示
    
    Test Plan:
    - 非强制更新
    - 更新提示：
    -- 优化页面显示效果，优化了交互体验
    
    Subscribers: liufengjun
    
    Differential Revision: https://dx.wangcaigu.cn/D3671

--- api/modules/v1/controllers/app/DeployController.php
+++ api/modules/v1/controllers/app/DeployController.php
@@ -34,23 +34,23 @@ class DeployController extends Controller
         ];
 
         if ('ios' === $clienttype) {
-            if ($versionCode < 18) {
+            if ($versionCode < 21) {
                 $content = [
                     'old_updatetype' => 2,  //旧版本更新类型 0:无更新;1:有更新,无提示;2:有更新,有提示;3:有更新,强制更新
-                    'updatedesc' => '优化了好友分享功能，优化页面显示效果',  //新版本更新说明
-                    'versioncode' => '18',   //新版本版本号
-                    'versionname' => '1.9', //新版本版本名称
+                    'updatedesc' => '优化页面显示效果，优化了交互体验',  //新版本更新说明
+                    'versioncode' => '21',   //新版本版本号
+                    'versionname' => '2.1', //新版本版本名称
                     'downloadurl' => 'https://itunes.apple.com/us/app/wen-dou-jin-fu/id1107540109?mt=8',   //新版本下载地址
                 ];
             }
         } else {
-            if ($versionCode < 18) {
+            if ($versionCode < 21) {
                 $content = [
                     'old_updatetype' => 2,  //旧版本更新类型 0:无更新;1:有更新,无提示;2:有更新,有提示;3:有更新,强制更新
-                    'updatedesc' => '优化了好友分享功能，优化页面显示效果',  //新版本更新说明
-                    'versioncode' => '18',   //新版本版本号
-                    'versionname' => '1.9', //新版本版本名称
-                    'downloadurl' => 'https://dapp.wenjf.com/wjf_v1.9.apk',   //新版本下载地址, android 不支持 http 向 https 的跳转，协议必须严格匹配
+                    'updatedesc' => '优优化页面显示效果，优化了交互体验',  //新版本更新说明
+                    'versioncode' => '21',   //新版本版本号
+                    'versionname' => '2.1', //新版本版本名称
+                    'downloadurl' => 'https://dapp.wenjf.com/wjf_2.1.apk',   //新版本下载地址, android 不支持 http 向 https 的跳转，协议必须严格匹配
                 ];
             }
         }
