d6d0491da11ba47ee8e416a0f2c7f6f40231fdd4
zhusikun <zhusikun@lianhejinxin.com>
2018-03-29 14:05:30 +0800

    楚天m端新人首投活动页面修改(master分支)
    Summary: 楚天m端新人首投活动页面修改
    
    Test Plan:
    新人首投活动页面修改(点击首页面新人首投活动banner进入)
    1、温馨提示标题： 首投 文字修改为第一笔投资，添加（此时新手标不参与）
    2、参与活动标题下的，作同上修改。
    
    Reviewers: modi
    
    Subscribers: zhusikun
    
    Maniphest Tasks: T2865
    
    Differential Revision: https://dx.wangcaigu.cn/D3718

--- view/themes/ctcf_new/m/modules/ctcf/views/landing/xs_invest.php
+++ view/themes/ctcf_new/m/modules/ctcf/views/landing/xs_invest.php
@@ -12,7 +12,7 @@ $this->title = '新人首投礼';
         <p class="title">任务详情</p>
         <p class="txt"><span>888元红包</span><i class="get">注册领取</i></p>
         <p class="txt"><span>50元超市卡</span><i class="get">首次投资领取</i></p>
-        <p class="sm-tips">温馨提示：首投≥5万,奖励可升级成160元超市卡哦！</p>
+        <p class="sm-tips">温馨提示：第一笔投资≥5万（此时新手标不参与），奖励可升级成160元超市卡哦！</p>
     </div>
     <a href="<?php if (Yii::$app->user->isGuest) { ?>/site/signup<?php } else { ?>/deal/deal/index<?php } ?>" class="common-btn get-btn">直接领取奖励</a>
     <img src="/ctcf/images/active-20180207/rule-title.png" alt="" class="wrap-img" />
@@ -25,7 +25,7 @@ $this->title = '新人首投礼';
         <li class="detail time">2018年2月23日起。</li>
         <li class="title">参与活动：</li>
         <li class="detail ">新人注册即可获得<span class="light-txt">888元红包</span>；<br/>新人首次投资任意金额，即可获得<span class="light-txt">1400积分</span>，您可以在积分商城兑换<span class="light-txt">50元超市卡</span>；</li>
-        <li class="detail">新人首投<span class="light-txt">≥5万元</span>，奖励将升级为<span class="light-txt">3500积分</span>，可在积分商城兑换<span class="light-txt">160元超市卡</span>；</li>
+        <li class="detail">新人第一笔投资<span class="light-txt">≥5万元</span>（此时新手标不参与），奖励将升级为<span class="light-txt">3500积分</span>，可在积分商城兑换<span class="light-txt">160元超市卡</span>；</li>
         <li class="detail time">超市卡每日限量，以兑换的时间先后顺序进行审核发放。</li>
         <li class="title ">领取奖励：</li>
         <li class="detail">完成活动任务后，红包与积分即时发放到账，您可在账户中心查看；</li>
